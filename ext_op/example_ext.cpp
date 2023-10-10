dc2cb0141abeea603f1a31bbf1bae0bdf03d5abd
gqw <994321849@qq.com>
2023-10-10 19:27:33 +0800

    change include file

--- ext_op/example_ext.cpp
+++ ext_op/example_ext.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 #include <diopi/functions.h>
-#include <diopi/functions_mmcv.h>
+#include <diopi/functions_ext.h>
 
 #include "csrc_dipu/diopirt/diopirt_impl.h"
 #include "csrc_dipu/base/basedef.h"
@@ -37,8 +37,9 @@ using dipu::diopi_helper::toDiopiTensorHandle;
 //     return torch::Tensor();
 // }
 
-void ext_apply_rotary(const torch::Tensor x1, const torch::Tensor x2,const torch::Tensor cos, const torch::Tensor sin,
-                       torch::Tensor out1, torch::Tensor out2, const bool conj) {
+void ext_apply_rotary(const torch::Tensor x1, const torch::Tensor x2, const torch::Tensor cos, const torch::Tensor sin,
+                      torch::Tensor out1, torch::Tensor out2, const bool conj)
+{
     auto out1_p = toDiopiTensorHandle(out1);
     auto out2_p = toDiopiTensorHandle(out2);
     auto cos_p = toDiopiTensorHandle(cos);
@@ -50,26 +51,28 @@ void ext_apply_rotary(const torch::Tensor x1, const torch::Tensor x2,const torch
     diopiContext ctx(dipu::getCurrentDIPUStream().rawstream());
     diopiContextHandle_t ch = &ctx;
 
-
-    if (device == diopi_host || x1.device().type() != dipu::DIPU_DEVICE_TYPE) {
-        std::cout<<"We only can run this op on dipu!"<<std::endl;
+    if (device == diopi_host || x1.device().type() != dipu::DIPU_DEVICE_TYPE)
+    {
+        std::cout << "We only can run this op on dipu!" << std::endl;
         throw "We only can run this op on dipu!";
     }
 
     auto ret =
-        diopiRotaryEmbedding(ch, out1_p, out2_p, x1_p, x2_p, cos_p, sin_p, conj);  // 此处更换为diopi内相应的函数
-    if (ret == diopiSuccess) {
+        diopiRotaryEmbedding(ch, out1_p, out2_p, x1_p, x2_p, cos_p, sin_p, conj); // 此处更换为diopi内相应的函数
+    if (ret == diopiSuccess)
+    {
         // auto tensorhandle = reinterpret_cast<torch::Tensor*>(*outhandle);
-        return ;
+        return;
     }
     throw "diopicalculate failed!";
-    return ;
+    return;
 }
 
 // 判断是否有对应的diopi实现，如果有，则直接pybind上去。如果没有，则不注册，再到python层处理。
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
+{
     // if(reinterpret_cast<void*>(diopiNmsMmcv) != nullptr)
     //     m.def("m_nms", &nms_diopi, "deeplink nms");
-    if(reinterpret_cast<void*>(diopiRotaryEmbedding) != nullptr)
+    if (reinterpret_cast<void *>(diopiRotaryEmbedding) != nullptr)
         m.def("apply_rotary", &ext_apply_rotary, "deeplink ext_apply_rotary");
 }
