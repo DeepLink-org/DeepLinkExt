b1b798038dbb23dc10e2374468824ea2aab44e0a
wey-code <1060314685@qq.com>
2023-10-12 16:22:51 +0800

    update RMSNorm autograd

---
+++ test/test_rms_internlm.py
@@ -0,0 +1,42 @@
+from ext_apply.RMSNorm import InternLMRMSNorm, DeeplinkRMSNorm
+import torch
+from torch import nn
+import torch_dipu
+
+
+
+
+input = torch.randn(5, 5, requires_grad=True).cuda()
+input_dipu = input.clone()
+hidden_size = 5
+
+
+intern = InternLMRMSNorm(hidden_size).cuda()
+deep = DeeplinkRMSNorm(hidden_size).cuda()
+a = intern(input)
+b = deep(input_dipu)
+print(a)
+print(b)
+
+c = torch.ones_like(a)
+
+
+# 1. 定义一个简单的损失函数
+loss_fn = torch.nn.MSELoss()
+
+loss = loss_fn(a, c)
+input.retain_grad()
+loss.backward()
+print("\nGradient for 'input':")
+input_grad = input.grad
+print(input_grad)
+
+loss2 = loss_fn(b, c)
+input_dipu.retain_grad()
+loss2.backward()
+print("\nGradient for 'input_dipu':")
+input_dipu_grad = input_dipu.grad
+print(input_dipu_grad)
+
+# 对比两者是否一致
+print("\nAre the gradients identical?:", torch.allclose(input_grad, input_dipu_grad))
