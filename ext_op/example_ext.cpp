e301300ebef8a93a43002897c9e5aadb1eeca9b4
lc shen <60810292+wey-code@users.noreply.github.com>
2023-10-12 14:52:09 +0800

    Slc/rmsnorm (#6)
    加入基础rmsnorm算子
    TODO：将其包装到前后向
    * add rms_norm
    
    * add rmsnorm ext op
    
    * move test files
    
    * fix test dipu case

--- ext_op/example_ext.cpp
+++ ext_op/example_ext.cpp
@@ -1,4 +1,5 @@
 #include <torch/extension.h>
+#include <c10/util/OptionalArrayRef.h>
 
 #include <iostream>
 
@@ -7,35 +8,93 @@
 
 #include "csrc_dipu/diopirt/diopirt_impl.h"
 #include "csrc_dipu/base/basedef.h"
+#include "m_pybind.h"
+
+
 
 using dipu::diopi_helper::toDiopiScalar;
 using dipu::diopi_helper::toDiopiTensorHandle;
+using dipu::diopi_helper::toDiopiSize;
 
-// torch::Tensor nms_diopi(torch::Tensor boxes, torch::Tensor scores, float iou_threshold, int offset) {
-//     auto boxes_p = toDiopiTensorHandle(boxes);
-//     diopiDevice_t device;
-//     diopiGetTensorDevice(boxes_p, &device);
-//     diopiContext ctx(dipu::getCurrentDIPUStream().rawstream());
-//     diopiContextHandle_t ch = &ctx;
-//     torch::Tensor out;
-//     auto outp = toDiopiTensorHandle(out);
-//     diopiTensorHandle_t* outhandle = &outp;
-//     auto scores_p = toDiopiTensorHandle(scores);
 
-//     if (device == diopi_host || boxes.device().type() != dipu::DIPU_DEVICE_TYPE) {
-//         std::cout<<"We only can run this op on dipu!"<<std::endl;
-//         throw "We only can run this op on dipu!";
-//     }
 
-//     auto ret =
-//         diopiNmsMmcv(ch, outhandle, boxes_p, scores_p, iou_threshold, offset);  // 此处更换为diopi内相应的函数
-//     if (ret == diopiSuccess) {
-//         auto tensorhandle = reinterpret_cast<torch::Tensor*>(*outhandle);
-//         return *tensorhandle;
-//     }
-//     throw "diopicalculate failed!";
-//     return torch::Tensor();
-// }
+std::tuple<at::Tensor, at::Tensor> ext_rms_norm(const torch::Tensor input, at::OptionalIntArrayRef normalized_shape, 
+                    const torch::Tensor weight, const torch::Tensor bias, double eps) {
+    normalized_shape = weight.sizes();
+    auto inv_rms = at::empty_like(input);
+    auto output = at::empty_like(input);
+    
+    auto input_p = toDiopiTensorHandle(input);
+    auto output_p = toDiopiTensorHandle(output);
+    auto inv_rms_p = toDiopiTensorHandle(inv_rms);
+    auto bias_p = toDiopiTensorHandle(bias);
+    auto weight_p = toDiopiTensorHandle(weight);
+    auto normalized_shape_p = toDiopiSize(normalized_shape);
+
+    diopiDevice_t device;
+    diopiGetTensorDevice(input_p, &device);
+    diopiContext ctx(dipu::getCurrentDIPUStream().rawstream());
+    diopiContextHandle_t ch = &ctx;
+
+    if (device == diopi_host || input.device().type() != dipu::DIPU_DEVICE_TYPE)
+    {
+        std::cout << "We only can run this op on dipu!" << std::endl;
+        throw "We only can run this op on dipu!";
+    }
+
+    // std::cout<<input.device().type()<<std::endl;
+    // std::cout<<dipu::DIPU_DEVICE_TYPE<<std::endl;
+
+    auto ret =
+        diopiRMSNorm(ch, output_p, inv_rms_p, input_p, normalized_shape_p, weight_p, bias_p, eps);  // 此处更换为diopi内相应的函数
+    if (ret == diopiSuccess) {
+        return std::tie(output, inv_rms);
+    }
+    throw "diopicalculate failed!";
+    return std::tie(output, inv_rms);
+}
+
+std::tuple<at::Tensor, at::Tensor, at::Tensor> ext_rms_norm_backward(const torch::Tensor& input, const torch::Tensor& grad_output,
+                     const torch::Tensor& inv_rms, at::OptionalIntArrayRef normalized_shape, const torch::Tensor& weight, 
+                     const torch::Tensor& bias, double eps = 1e-6) {
+    
+    normalized_shape = weight.sizes();
+    // std::cout<<*normalized_shape<<std::endl;
+
+    auto grad_input = at::empty_like(grad_output);
+    auto grad_weight = at::empty_like(weight);
+    auto grad_bias = at::empty_like(bias);
+    
+    auto input_p = toDiopiTensorHandle(input);
+    auto grad_output_p = toDiopiTensorHandle(grad_output);
+    auto inv_rms_p = toDiopiTensorHandle(inv_rms);
+    auto bias_p = toDiopiTensorHandle(bias);
+    auto weight_p = toDiopiTensorHandle(weight);
+    auto normalized_shape_p = toDiopiSize(normalized_shape);
+    auto grad_input_p = toDiopiTensorHandle(grad_input);
+    auto grad_weight_p = toDiopiTensorHandle(grad_weight);
+    auto grad_bias_p = toDiopiTensorHandle(grad_bias);
+
+    diopiDevice_t device;
+    diopiGetTensorDevice(input_p, &device);
+    diopiContext ctx(dipu::getCurrentDIPUStream().rawstream());
+    diopiContextHandle_t ch = &ctx;
+
+    if (device == diopi_host || input.device().type() != dipu::DIPU_DEVICE_TYPE)
+    {
+        std::cout << "We only can run this op on dipu!" << std::endl;
+        throw "We only can run this op on dipu!";
+    }
+
+    auto ret =
+        diopiRMSNormBackward(ch, grad_input_p, grad_weight_p, grad_bias_p, grad_output_p, input_p, weight_p, bias_p, inv_rms_p, normalized_shape_p, eps);  // 此处更换为diopi内相应的函数
+    if (ret == diopiSuccess) {
+        return std::tie(grad_input, grad_weight, grad_bias);
+    }
+    throw "diopicalculate failed!";
+    return std::tie(grad_input, grad_weight, grad_bias);
+}
+
 
 void ext_apply_rotary(const torch::Tensor x1, const torch::Tensor x2, const torch::Tensor cos, const torch::Tensor sin,
                       torch::Tensor out1, torch::Tensor out2, const bool conj)
@@ -71,8 +130,10 @@ void ext_apply_rotary(const torch::Tensor x1, const torch::Tensor x2, const torc
 // 判断是否有对应的diopi实现，如果有，则直接pybind上去。如果没有，则不注册，再到python层处理。
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
 {
-    // if(reinterpret_cast<void*>(diopiNmsMmcv) != nullptr)
-    //     m.def("m_nms", &nms_diopi, "deeplink nms");
-    if (reinterpret_cast<void *>(diopiRotaryEmbedding) != nullptr)
+    if(reinterpret_cast<void*>(diopiRMSNorm) != nullptr)
+        m.def("rms_norm", &ext_rms_norm, "deeplink ext_rms_norm");
+    if(reinterpret_cast<void*>(diopiRMSNormBackward) != nullptr)
+        m.def("rms_norm_backward", &ext_rms_norm_backward, "deeplink ext_rms_norm_backward");
+    if(reinterpret_cast<void*>(diopiRotaryEmbedding) != nullptr)
         m.def("apply_rotary", &ext_apply_rotary, "deeplink ext_apply_rotary");
 }
---
+++ test/test_rms.py
@@ -0,0 +1,53 @@
+import torch
+
+import dipu_ext.ext_ as deeplink_ext
+import torch_dipu
+import pdb
+# import debugat
+
+# 假设 deeplink_ext 是一个包含上述 RMS normalization 函数的模块
+
+# 定义输入张量
+input = torch.randn(5, 5, requires_grad=True).cuda()
+
+# 定义权重和偏差张量
+weight = torch.randn(5, requires_grad=True).cuda()
+bias = torch.randn(5, requires_grad=True).cuda()
+
+# 定义输出梯度张量
+grad_output = torch.randn(5, 5).cuda()
+
+# 归一化的形状通常是输入张量的形状
+normalized_shape = torch.tensor([5, 5], dtype=torch.long).cuda()
+
+# pdb.set_trace()
+
+# 使用 RMS normalization 前向传播
+# while True:
+
+print(input.is_dipu)
+output, inv_rms = deeplink_ext.rms_norm(
+    input,
+    None,
+    weight,
+    bias,
+    1e-6
+)
+
+# 使用 RMS normalization 反向传播
+grad_input, grad_weight, grad_bias = deeplink_ext.rms_norm_backward(
+    input,
+    grad_output,
+    inv_rms,
+    None,
+    weight,
+    bias,
+    1e-6
+)
+
+print("Output:", output)
+print("Grad Input:", grad_input)
+print("Grad Weight:", grad_weight)
+print("Grad Bias:", grad_bias)
+b = input * torch.rsqrt(input.pow(2).mean(-1, keepdim=True) + 1e-6) * weight
+assert torch.allclose(output, b)
