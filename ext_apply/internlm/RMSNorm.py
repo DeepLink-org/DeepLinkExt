a2b059f4154cc89f40bcc59b8e4900b2f836cb1f
Lingjie <lljbash@gmail.com>
2023-10-16 17:14:16 +0800

    fix py::List -> at::OptionalIntArrayRef lifetime (#12)
    * fix py::List -> at::OptionalIntArrayRef lifetime
    
    * Adding the test case of rms, they tested when normalized shape is equal to None or weight.size(). (#13)
    
    ---------
    
    Co-authored-by: lc shen <60810292+wey-code@users.noreply.github.com>

--- ext_apply/internlm/RMSNorm.py
+++ ext_apply/internlm/RMSNorm.py
@@ -54,6 +54,37 @@ class _DeeplinkRMSNormFunction(torch.autograd.Function):
             eps
         )
         return grad_input, grad_weight, grad_bias, None
+    
+class _DeeplinkRMSNormFunction_WithNormalizedShape(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, hidden_states, weight, bias, eps, normalized_shape):
+        output, inv_rms = deeplink_ext.rms_norm(
+            hidden_states,
+            normalized_shape,
+            weight,
+            bias,
+            eps
+        )
+
+        ctx.save_for_backward(hidden_states, inv_rms, weight, bias, torch.tensor(eps))
+        ctx.intermediate_results = normalized_shape
+        return output
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        hidden_states, inv_rms, weight, bias, eps_tensor = ctx.saved_tensors
+        eps = eps_tensor.item()
+        normalized_shape = ctx.intermediate_results
+        grad_input, grad_weight, grad_bias = deeplink_ext.rms_norm_backward(
+            hidden_states,
+            grad_output,
+            inv_rms,
+            normalized_shape,
+            weight,
+            bias,
+            eps
+        )
+        return grad_input, grad_weight, grad_bias, None, None
 
 
 # 定义一个nn.Module包裹这个自定义函数
@@ -67,3 +98,14 @@ class DeeplinkRMSNorm(nn.Module):
 
     def forward(self, hidden_states):
         return _DeeplinkRMSNormFunction.apply(hidden_states, self.weight, self.bias, self.variance_epsilon)
+    
+class DeeplinkRMSNorm_WithNormalizedShape(nn.Module):
+
+    def __init__(self, hidden_size, eps=1e-6):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(hidden_size))
+        self.bias = nn.Parameter(torch.zeros(hidden_size))
+        self.variance_epsilon = eps
+
+    def forward(self, hidden_states):
+        return _DeeplinkRMSNormFunction_WithNormalizedShape.apply(hidden_states, self.weight, self.bias, self.variance_epsilon, self.weight.size())
--- ext_op/m_pybind.h
+++ ext_op/m_pybind.h
@@ -1,61 +1,38 @@
 #pragma once
 
-// 这个目前存在bug，生成的IntArrayRefToPyObject似乎传过去就不对了，后续再解决
+#include <vector>
 
-#include <pybind11/pybind11.h>
-#include <torch/types.h>
-#include <torch/python.h> 
-#include <c10/util/OptionalArrayRef.h>
+#include "pybind11/pybind11.h"
+#include "c10/util/ArrayRef.h"
+#include "c10/util/Optional.h"
+#include "torch/types.h"
+
+namespace dipu_ext {
+
+using IntArray = std::vector<at::IntArrayRef::value_type>;
+using OptionalIntArray = c10::optional<IntArray>;
+
+} // namespace dipu_ext
 
 namespace pybind11 {
 namespace detail {
 
 namespace py = pybind11;
 
-at::OptionalIntArrayRef PyObjectToOptionalIntArrayRef(PyObject* obj) {
-    if (obj == Py_None) {
-        return at::OptionalIntArrayRef();
-    }
-
-    // Assuming obj is a PyList
-    std::vector<int64_t> vec = py::cast<std::vector<int64_t>>(obj);
-    return at::OptionalIntArrayRef(vec);
-}
-
-PyObject* OptionalIntArrayRefToPyObject(const at::OptionalIntArrayRef& ref) {
-    if (!ref.has_value()) {
-        Py_RETURN_NONE;
-    }
-    auto array_ref = ref.value();
-    return py::cast(std::vector<int64_t>(array_ref.data(), array_ref.data() + array_ref.size())).release().ptr();
-}
-
-
 template <>
 struct type_caster<at::OptionalIntArrayRef> {
 public:
-    PYBIND11_TYPE_CASTER(at::OptionalIntArrayRef, _("OptionalIntArrayRef"));
+    PYBIND11_TYPE_CASTER(dipu_ext::OptionalIntArray, _("OptionalIntArray"));
 
     bool load(py::handle src, bool) {
         if (PyList_Check(src.ptr())) {
-            std::vector<int64_t> vec = py::cast<std::vector<int64_t>>(src);
-            at::ArrayRef<int64_t> array_ref(vec.data(), 2);
-            at::OptionalIntArrayRef v = at::OptionalIntArrayRef(array_ref);
-            value = v;
-            // // std::cout<<vec[0]<<std::endl;
-            // std::cout<<array_ref<<std::endl;
-            // std::cout<<*v<<std::endl;
-            // std::cout<<bool(array_ref==v)<<std::endl;
+            value = py::cast<dipu_ext::IntArray>(src);
             return true;
         } else if (src.is_none()) {
-            value = at::OptionalIntArrayRef();  // Assigning an empty OptionalArrayRef
+            value = c10::nullopt;
             return true;
         }
         return false;
-    }
-
-    static py::handle cast(const at::OptionalIntArrayRef& src, py::return_value_policy /* policy */, py::handle /* parent */) {
-        return py::handle(OptionalIntArrayRefToPyObject(src));
     }
 };
 
