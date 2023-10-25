9a5e72d1d90df3042cc5661651c676546b43ce10
gqw <994321849@qq.com>
2023-10-25 17:48:17 +0800

    bug fix

--- ext_apply/internlm/ext_apply_rotary.py
+++ ext_apply/internlm/ext_apply_rotary.py
@@ -89,22 +89,23 @@ try:
             sin_k = sin if sin_k is None else sin_k
             assert sin.shape == cos_k.shape == sin_k.shape == (rotary_seqlen, rotary_dim // 2)
             q_ro = qkv[:, :, 0, :, :rotary_dim]
-            dipu_ext.ext_.apply_rotary(q_ro, q_ro,  rearrange(cos[:seqlen], "s d -> s 1 d"), rearrange(sin[:seqlen], "s d -> s 1 d"), False)
+            dipu_ext.ext_.apply_rotary(q_ro, q_ro,  rearrange(cos[:seqlen], "s d -> s 1 d"), rearrange(sin[:seqlen], "s d -> s 1 d"), False, interleaved)
             k_ro = qkv[:, :, 1, :, :rotary_dim]
-            dipu_ext.ext_.apply_rotary(k_ro, k_ro,  rearrange(cos[:seqlen], "s d -> s 1 d"), rearrange(sin[:seqlen], "s d -> s 1 d"), False)
+            dipu_ext.ext_.apply_rotary(k_ro, k_ro,  rearrange(cos[:seqlen], "s d -> s 1 d"), rearrange(sin[:seqlen], "s d -> s 1 d"), False, interleaved)
             ctx.save_for_backward(cos, sin, cos_k, sin_k)
             ctx.interleaved = interleaved
             return qkv
         @staticmethod
         def backward(ctx, dqkv):
             cos, sin, cos_k, sin_k = ctx.saved_tensors
+            interleaved = ctx.interleaved
             _, seqlen, _, _, headdim = dqkv.shape
             rotary_dim = cos.shape[-1]
             rotary_dim *= 2
             dq_ro = dqkv[:, :, 0, :, :rotary_dim]
-            dipu_ext.ext_.apply_rotary(dq_ro, dq_ro,  rearrange(cos[:seqlen], "s d -> s 1 d"), rearrange(sin[:seqlen], "s d -> s 1 d"), True)
+            dipu_ext.ext_.apply_rotary(dq_ro, dq_ro,  rearrange(cos[:seqlen], "s d -> s 1 d"), rearrange(sin[:seqlen], "s d -> s 1 d"), True, interleaved)
             dk_ro = dqkv[:, :, 1, :, :rotary_dim]
-            dipu_ext.ext_.apply_rotary(dk_ro, dk_ro,  rearrange(cos[:seqlen], "s d -> s 1 d"), rearrange(sin[:seqlen], "s d -> s 1 d"), True)
+            dipu_ext.ext_.apply_rotary(dk_ro, dk_ro,  rearrange(cos[:seqlen], "s d -> s 1 d"), rearrange(sin[:seqlen], "s d -> s 1 d"), True, interleaved)
             return dqkv, None, None, None, None, None
 except:
     print("NOT USING ext apply_rotary")
