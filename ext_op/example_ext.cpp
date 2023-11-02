b3bdea3989f531cf2df1d5d056ad143abd3e1896
wey-code <1060314685@qq.com>
2023-11-02 18:34:40 +0800

    fix lint

--- ext_op/example_ext.cpp
+++ ext_op/example_ext.cpp
@@ -74,10 +74,9 @@ void extApplyRotary(at::Tensor output, const at::Tensor& input,
   callDiopi(diopiRotaryEmbedding, output, input, cos, sin, conj, interleaved);
 }
 
-auto extMultiHeadAttention(at::Tensor& q, at::Tensor& k,
-                           at::Tensor& v, double dropout_p,
-                           bool is_casual, bool return_debug_mask,
-                           double scale) {
+auto extMultiHeadAttention(at::Tensor& q, at::Tensor& k, at::Tensor& v,
+                           double dropout_p, bool is_casual,
+                           bool return_debug_mask, double scale) {
   const auto batch_size = q.sizes()[0];
   const auto q_seq_len = q.sizes()[1];
   const auto head_num = q.sizes()[2];
