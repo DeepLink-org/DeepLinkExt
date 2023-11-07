6a289be16f08da3bc75ab0092bb87225c66f11bf
ZhangQiu <100055343+NeosZhang@users.noreply.github.com>
2023-11-07 19:35:09 +0800

    Update mock_op.py
    使用环境变量DIOPI_OP_LIST来管理替换的diopi实现

--- ext_apply/lightllm/mock_op.py
+++ ext_apply/lightllm/mock_op.py
@@ -1,29 +1,29 @@
 import torch
 import os
 try:
-    #export DISABLE_DIOPI_OP_LIST=dest_index_copy_kv,token_attention_inference,token_softmax_reducev_inference
+    #export DIOPI_OP_LIST=dest_index_copy_kv,token_attention_inference,token_softmax_reducev_inference,context_attention_inference,apply_penalty
     import dipu_ext.ext_ as ext
-    disable_diopi_op_list = os.environ['DISABLE_DIOPI_OP_LIST'].split(',')
-    print(f"DISABLE_DIOPI_OP_LIST:{disable_diopi_op_list}")
-    if hasattr(ext, 'dest_index_copy_kv') and 'dest_index_copy_kv' not in disable_diopi_op_list:
+    diopi_op_list = os.environ['DIOPI_OP_LIST'].split(',')
+    print(f"DIOPI_OP_LIST:{diopi_op_list}")
+    if hasattr(ext, 'dest_index_copy_kv') and 'dest_index_copy_kv' in diopi_op_list:
         import lightllm.common.basemodel.triton_kernel.destindex_copy_kv as destindex_copy_kv_pack
         destindex_copy_kv_pack.destindex_copy_kv = ext.dest_index_copy_kv
         print("use diopi dest_index_copy_kv as destindex_copy_kv")
 
-    if hasattr(ext, 'apply_penalty') and 'apply_penalty' not in disable_diopi_op_list:
+    if hasattr(ext, 'apply_penalty') and 'apply_penalty' in diopi_op_list:
         import lightllm.common.basemodel.triton_kernel.apply_penalty as apply_penalty_pack
         apply_penalty_pack.apply_penalty = ext.apply_penalty
         print("use diopi apply_penalty")
 
-    if hasattr(ext, 'context_attention_inference') and 'context_attention_inference' not in disable_diopi_op_list:
+    if hasattr(ext, 'context_attention_inference') and 'context_attention_inference'  in diopi_op_list:
         import lightllm.models.llama.triton_kernel.context_flashattention_nopad as context_attention_pack
         context_attention_pack.context_attention_fwd = ext.context_attention_inference
         print("use diopi context_attention_inference as context_attention_fwd")
-    if hasattr(ext, 'token_attention_inference') and 'token_attention_inference' not in disable_diopi_op_list:
+    if hasattr(ext, 'token_attention_inference') and 'token_attention_inference' in diopi_op_list:
         import lightllm.models.llama.triton_kernel.token_attention_nopad_att1 as token_attention_pack
         token_attention_pack.token_att_fwd = ext.token_attention_inference
         print("use diopi token_attention_inference as token_att_fwd")
-    if hasattr(ext, 'token_softmax_reducev_inference') and 'token_softmax_reducev_inference' not in disable_diopi_op_list:
+    if hasattr(ext, 'token_softmax_reducev_inference') and 'token_softmax_reducev_inference' in diopi_op_list:
         import lightllm.models.llama.triton_kernel.token_attention_softmax_and_reducev as token_attention_softmax_reducev_pack
         token_attention_softmax_reducev_pack.token_softmax_reducev_fwd = ext.token_softmax_reducev_inference
         print("use diopi token_softmax_reducev_inference as token_softmax_reducev_fwd")
@@ -31,4 +31,3 @@ except ImportError:
     pass
 
 
-#ext.context_attention_inference(   apply_penalty   ext.dest_index_copy_kv(               ext.token_attention_inference(        ext.token_softmax_reducev_inference
