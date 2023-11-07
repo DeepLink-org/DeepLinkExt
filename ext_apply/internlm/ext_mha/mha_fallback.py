0dc2e760df3111c232df7b09a61269016c4ee12d
gqw <994321849@qq.com>
2023-11-07 15:15:10 +0800

    add varlen fallback

--- ext_apply/internlm/ext_mha/mha_fallback.py
+++ ext_apply/internlm/ext_mha/mha_fallback.py
@@ -2,7 +2,7 @@
 
 import torch
 import torch.nn as nn
-
+from einops import rearrange
 
 class SelfAttention(nn.Module):
     def __init__(self, causal=False, softmax_scale=None, attention_dropout=0.0):
@@ -11,13 +11,18 @@ class SelfAttention(nn.Module):
         self.softmax_scale = softmax_scale
         self.drop = nn.Dropout(attention_dropout)
 
-    def forward(self, qkv, causal=None):
-        """Only supports padded"""
-        seqlen = qkv.shape[1]
+    def forward(self, qkv, causal=None, key_padding_mask=None):
+        """supports padded and unpadded(using key_padding_mask)"""
+        batch_size, seqlen = qkv.shape[0], qkv.shape[1]
         causal = self.causal if causal is None else causal
         q, k, v = qkv.unbind(dim=2)
         softmax_scale = self.softmax_scale or q.shape[-1] ** -0.5
         scores = torch.einsum("bthd,bshd->bhts", q, k * softmax_scale)
+        if key_padding_mask is not None:
+            padding_mask = torch.full((batch_size, seqlen), -10000.0, dtype=scores.dtype,
+                                      device=scores.device)
+            padding_mask.masked_fill_(key_padding_mask, 0.0)
+            scores = scores + rearrange(padding_mask, 'b s -> b 1 1 s')
         if causal:
             causal_mask = torch.triu(
                 torch.full((seqlen, seqlen), -10000.0, device=scores.device), 1
