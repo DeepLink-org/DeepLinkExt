5befa869a2ed6ac5ff38fa9b05311b55d034085e
Lingjie Li <lljbash@gmail.com>
2024-01-31 20:54:59 +0800

    feat(rotary,internlm): add DeepLinkApplyRotaryEmb (fallback unimplemented)

--- deeplink_ext/internlm_ops/rotary/__init__.py
+++ deeplink_ext/internlm_ops/rotary/__init__.py
@@ -1,11 +1,14 @@
 # Copyright (c) 2024, DeepLink.
 
 try:
-    from .deeplink import DeepLinkApplyRotaryEmbQKV_
+    from .deeplink import DeepLinkApplyRotaryEmbQKV_, DeepLinkApplyRotaryEmb
 except:
     print(
         "[deeplink_ext] rotary is not implemented in diopi. Falling back to the slower implementation.\n",
         end="",
     )
-    from .fallback import ApplyRotaryEmbQKV_ as DeepLinkApplyRotaryEmbQKV_
+    from .fallback import (
+        ApplyRotaryEmbQKV_ as DeepLinkApplyRotaryEmbQKV_,
+        ApplyRotaryEmb as DeepLinkApplyRotaryEmb,
+    )
 from . import fallback
