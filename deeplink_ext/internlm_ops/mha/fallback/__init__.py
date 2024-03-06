3d42957f9801f9e58c39a28219622459d346c52c
Lingjie <lljbash@gmail.com>
2024-03-06 13:51:35 +0800

    feat(internlm): add force_fallback option to patch_internlm.py (#49)
    * feat(internlm): add force_fallback option to patch_internlm.py
    
    plus: fix RMSNorm not patched
    
    * style: blacken the patch_internlm.py file
    
    * refactor(internlm): tweak the patching logic to be more redundant
    
    * build: fix CI
    
    * feat(internlm): add fallback for RMSNorm with normalized shape
    
    * build(CI): fix the find command in static.yml

--- deeplink_ext/internlm_ops/mha/fallback/__init__.py
+++ deeplink_ext/internlm_ops/mha/fallback/__init__.py
@@ -1,3 +1,5 @@
 # Copyright (c) 2024, DeepLink.
 
 from .fallback import SelfAttention, CrossAttention
+
+__all__ = ["SelfAttention", "CrossAttention"]
--- deeplink_ext/internlm_ops/rms_norm/fallback/__init__.py
+++ deeplink_ext/internlm_ops/rms_norm/fallback/__init__.py
@@ -1,3 +1,5 @@
 # Copyright (c) 2024, DeepLink.
 
 from .fallback import RMSNorm
+
+__all__ = ["RMSNorm"]
