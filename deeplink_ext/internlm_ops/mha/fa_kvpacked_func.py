41e7e04bdceb87eb81af7cdcd034a06d51b59c2d
POI-WX <wangxing2@sensetime.com>
2024-03-13 15:39:31 +0800

    update

--- deeplink_ext/internlm_ops/mha/fa_kvpacked_func.py
+++ deeplink_ext/internlm_ops/mha/fa_kvpacked_func.py
@@ -11,24 +11,21 @@ class DeepLinkFlashAttentionKVPackedFunc(torch.autograd.Function):
     def forward(ctx, q, kv, dropout_p, softmax_scale, causal):
         if softmax_scale is None:
             softmax_scale = q.shape[-1] ** (-0.5)
-        out, softmax_max, softmax_sum, softmax_out, rng = ext.fa_fwd(
+        out, attention_mask, dropout_mask, softmax_max, softmax_sum, softmax_out = ext.fa_fwd(
             q, kv[:, :, 0], kv[:, :, 1], dropout_p, softmax_scale, causal
         )
         ctx.save_for_backward(
-            q, kv, out, softmax_max, softmax_sum, softmax_out, rng.get_state()
+            q, kv, out, attention_mask, dropout_mask, softmax_max, softmax_sum, softmax_out
         )
         ctx.dropout_p = dropout_p
         ctx.softmax_scale = softmax_scale
-        ctx.causal = causal
         return out
 
     @staticmethod
     def backward(ctx, dout):
-        q, kv, out, softmax_max, softmax_sum, softmax_out, rng_state = ctx.saved_tensors
+        q, kv, out, attention_mask, dropout_mask, softmax_max, softmax_sum, softmax_out = ctx.saved_tensors
         dq = torch.empty_like(q)
         dkv = torch.empty_like(kv)
-        rng = torch.Generator(device=q.device)
-        rng.set_state(rng_state)
         ext.fa_bwd(
             dq,
             dkv[:, :, 0],
@@ -38,12 +35,12 @@ class DeepLinkFlashAttentionKVPackedFunc(torch.autograd.Function):
             kv[:, :, 0],
             kv[:, :, 1],
             out,
+            attention_mask, 
+            dropout_mask,
             softmax_max,
             softmax_sum,
             softmax_out,
-            rng,
             ctx.dropout_p,
             ctx.softmax_scale,
-            ctx.causal,
         )
         return dq, dkv, None, None, None, None
--- deeplink_ext/internlm_ops/mha/fa_qkvpacked_func.py
+++ deeplink_ext/internlm_ops/mha/fa_qkvpacked_func.py
@@ -11,23 +11,20 @@ class DeepLinkFlashAttentionQKVPackedFunc(torch.autograd.Function):
     def forward(ctx, qkv, dropout_p, softmax_scale, causal):
         if softmax_scale is None:
             softmax_scale = qkv.shape[-1] ** (-0.5)
-        out, softmax_max, softmax_sum, softmax_out, rng = ext.fa_fwd(
+        out, attention_mask, dropout_mask, softmax_max, softmax_sum, softmax_out = ext.fa_fwd(
             qkv[:, :, 0], qkv[:, :, 1], qkv[:, :, 2], dropout_p, softmax_scale, causal
         )
         ctx.save_for_backward(
-            qkv, out, softmax_max, softmax_sum, softmax_out, rng.get_state()
+            qkv, out, attention_mask, dropout_mask, softmax_max, softmax_sum, softmax_out
         )
         ctx.dropout_p = dropout_p
         ctx.softmax_scale = softmax_scale
-        ctx.causal = causal
         return out
 
     @staticmethod
     def backward(ctx, dout):
-        qkv, out, softmax_max, softmax_sum, softmax_out, rng_state = ctx.saved_tensors
+        qkv, out, attention_mask, dropout_mask, softmax_max, softmax_sum, softmax_out = ctx.saved_tensors
         dqkv = torch.empty_like(qkv)
-        rng = torch.Generator(device=qkv.device)
-        rng.set_state(rng_state)
         ext.fa_bwd(
             dqkv[:, :, 0],
             dqkv[:, :, 1],
@@ -37,12 +34,12 @@ class DeepLinkFlashAttentionQKVPackedFunc(torch.autograd.Function):
             qkv[:, :, 1],
             qkv[:, :, 2],
             out,
+            attention_mask,
+            dropout_mask,
             softmax_max,
             softmax_sum,
             softmax_out,
-            rng,
             ctx.dropout_p,
             ctx.softmax_scale,
-            ctx.causal,
         )
         return dqkv, None, None, None, None
