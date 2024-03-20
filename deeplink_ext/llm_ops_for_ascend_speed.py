99406d9d1918b62c0649c8153becc5b064b986af
POI-WX <wangxing2@sensetime.com>
2024-03-20 15:59:23 +0800

    add rotary embedding for ascend speed

--- deeplink_ext/llm_ops_for_ascend_speed.py
+++ deeplink_ext/llm_ops_for_ascend_speed.py
@@ -1,9 +1,11 @@
 # Copyright (c) 2024, DeepLink.
 
+from typing import Optional, Union
 import torch
 import deeplink_ext.cpp_extensions as ext
 
 assert hasattr(ext, "fa_fwd") and hasattr(ext, "fa_bwd")
+assert hasattr(ext, "apply_rotary")
 
 
 class DeepLinkFlashSelfAttention(torch.autograd.Function):
@@ -80,3 +82,31 @@ class DeepLinkFlashSelfAttention(torch.autograd.Function):
             ctx.head_num,
         )
         return dq, dk, dv, None, None, None, None
+
+
+def apply_rotary_for_ascend_speed(
+    x: torch.Tensor,
+    cos: torch.Tensor,
+    sin: torch.Tensor,
+    seqlen_offsets: Union[int, torch.Tensor] = 0,
+    cu_seqlens: Optional[torch.Tensor] = None,
+    max_seqlen: Optional[int] = None,
+    interleaved=False,
+    inplace=False,
+    conjugate=False,
+) -> torch.Tensor:
+    output = torch.empty_like(x)
+    ext.apply_rotary(output, x, cos, sin, conjugate, interleaved)
+    return output
+
+
+class DeepLinkRotaryEmbedding(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, t, cos, sin):
+        ctx.save_for_backward(cos, sin)
+        return apply_rotary_for_ascend_speed(t, cos, sin)
+
+    @staticmethod
+    def backward(ctx, t):
+        cos, sin = ctx.saved_tensors
+        return apply_rotary_for_ascend_speed(t, cos, sin, conjugate=True), None, None
