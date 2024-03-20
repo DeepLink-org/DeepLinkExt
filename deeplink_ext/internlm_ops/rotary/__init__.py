e67d7b6be39800a08cbcc365337c50834c6bdb97
zhangzefeng92 <zhang_zefeng@foxmail.com>
2024-03-20 15:09:21 +0800

    adapt ascend speed

--- deeplink_ext/internlm_ops/rotary/__init__.py
+++ deeplink_ext/internlm_ops/rotary/__init__.py
@@ -1,13 +1,14 @@
 # Copyright (c) 2024, DeepLink.
 
 try:
-    from .deeplink import apply_rotary
+    from .deeplink import apply_rotary, RotaryEmbedding_AscendSpeed
 except:
     print(
         "[deeplink_ext] rotary is not implemented in diopi. Falling back to the slower implementation.\n",
         end="",
     )
     from .fallback import apply_rotary
+    RotaryEmbedding_AscendSpeed = None
 from . import fallback
 
-__all__ = ["apply_rotary", "fallback"]
+__all__ = ["apply_rotary", "fallback", "RotaryEmbedding_AscendSpeed"]
