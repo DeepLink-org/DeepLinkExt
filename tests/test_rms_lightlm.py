b0aa03d1eb384d9de969c1b668be3e37c10376e1
Zhangzefeng <zhang_zefeng@foxmail.com>
2024-03-27 09:08:52 +0800

    Update test_rms_lightlm.py

--- tests/test_rms_lightlm.py
+++ tests/test_rms_lightlm.py
@@ -17,9 +17,6 @@ grad_output = torch.randn(5, 5).cuda()
 normalized_shape = torch.tensor([5, 5], dtype=torch.long).cuda()
 
 print(input.is_dipu)
-# output, inv_rms = ext.rms_norm(input, None, weight, bias, 1e-6)
-# output1, inv_rms1 = ext.rms_norm(input, weight.shape, weight, bias, 1e-6)
-
 output = torch.empty_like(input)
 inv_rms_shape = list(input.shape[:-1]) + [1]
 inv_rms = torch.empty(inv_rms_shape, dtype=torch.float32, device=input.device)
@@ -44,9 +41,5 @@ ext.rms_norm_backward(
     1e-6
 )
 
-# print("Output:", output)
-# print("Grad Input:", grad_input)
-# print("Grad Weight:", grad_weight)
-# print("Grad Bias:", grad_bias)
 b = input * torch.rsqrt(input.pow(2).mean(-1, keepdim=True) + 1e-6) * weight
 assert torch.allclose(output, b)
