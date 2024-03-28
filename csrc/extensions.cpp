f0eed5aae2f7f37649dc2719e0d70942b4ad9de6
zhangzefeng92 <zhang_zefeng@foxmail.com>
2024-03-28 14:02:19 +0800

    modify rotary emb op

--- csrc/extensions.cpp
+++ csrc/extensions.cpp
@@ -234,16 +234,6 @@ auto extRmsNormLightllm(const at::Tensor& x, const at::Tensor& weight,
   return out;
 }
 
-// For lightllm, rotary_embedding reuses the diopi implementation of internlm
-void extRotaryEmb(at::Tensor& q, const at::Tensor& cos, const at::Tensor& sin) {
-  auto seq_len = q.size(0);
-  auto dim = q.size(-1);
-  auto cos_view = cos.view({seq_len, 1, dim / 2});
-  auto sin_view = sin.view({seq_len, 1, dim / 2});
-  callDiopi(diopiRotaryEmbedding, q, q, cos_view, sin_view, /*conj=*/false,
-            /*interleaved=*/false);
-}
-
 // 判断是否有对应的 diopi 实现:
 //   如果有, 则直接 pybind 上去;
 //   否则不注册, 等到 python 层处理.
@@ -261,7 +251,6 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   }
   if (&diopiRotaryEmbedding != nullptr) {
     m.def("apply_rotary", &extApplyRotary, "deeplink ext_apply_rotary");
-    m.def("rotary_emb", &extRotaryEmb, "deeplink ext_rotary_emb for lightllm");
   }
   if (&diopiMultiHeadAttention != nullptr) {
     m.def("mha_fwd", &extMultiHeadAttention, "deeplink ext_mha_fwd");
--- tests/test_rotary_emb_internlm.py
+++ tests/test_rotary_emb_internlm.py
@@ -3,8 +3,7 @@
 import torch
 import deeplink_ext.internlm_ops.rotary as ext
 
-
-def RotaryEmbTest() -> bool:
+def RotaryEmbTestFloat16() -> bool:
     input = torch.randn(1, 125, 16, 32, dtype=torch.float16).cuda()
 
     cos = torch.randn(217, 16, dtype=torch.float16).cuda()
@@ -18,7 +17,25 @@ def RotaryEmbTest() -> bool:
     )
     res2 = ext.apply_rotary(input1, cos, sin, interleaved=interleaved, inplace=inplace)
 
+    # there is a little calculated error with ascend
+    return torch.allclose(res1, res2, atol=1e-2, rtol=1e-3)
+
+def RotaryEmbTestFloat32() -> bool:
+    input = torch.randn(1, 125, 16, 32, dtype=torch.float32).cuda()
+
+    cos = torch.randn(217, 16, dtype=torch.float32).cuda()
+    sin = torch.randn(217, 16, dtype=torch.float32).cuda()
+    input1 = input.detach().clone()
+    inplace = True
+    interleaved = False
+
+    res1 = ext.fallback.apply_rotary(
+        input, cos, sin, interleaved=interleaved, inplace=inplace
+    )
+    res2 = ext.apply_rotary(input1, cos, sin, interleaved=interleaved, inplace=inplace)
+
     return torch.allclose(res1, res2)
 
 
-assert RotaryEmbTest()
+assert RotaryEmbTestFloat32()
+assert RotaryEmbTestFloat16()
