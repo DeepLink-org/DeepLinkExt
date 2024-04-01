7ce2b22b546f9806754afb7b309ba08fc249ade4
root <yangbofun@163.com>
2024-04-01 05:10:01 +0000

    modify rotary_embeding

--- deeplink_ext/internlm_ops/__init__.py
+++ deeplink_ext/internlm_ops/__init__.py
@@ -1,14 +1,29 @@
 # Copyright (c) 2024, DeepLink.
 
-from . import mha, rotary
+from . import mha
+
+
+_not_impl = "[deeplink_ext] %s is not implemented in diopi. Falling back to the slower torch implementation."
+
 
 try:
     from .rms_norm import RMSNorm, RMSNormWithNormalizedShape
 except:
+    print(
+        _not_impl.format("RMSNorm or RMSNormWithNormalizedShape"),
+    )
     from .rms_norm_fallback import (
         RMSNorm as RMSNorm,
         RMSNorm as RMSNormWithNormalizedShape,
     )
 
 
-__all__ = ["mha", "rotary", RMSNorm, RMSNormWithNormalizedShape]
+try:
+    from .rotary_embedding import apply_rotary
+except:
+    print( _not_impl.format("apply_rotary"))
+    from .rotary_embeddinig_fallback import apply_rotary
+
+
+
+__all__ = ["mha", "RMSNorm", "RMSNormWithNormalizedShape", "apply_rotary"]
