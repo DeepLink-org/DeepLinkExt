52bc92709d9e3b0ccb5b9ed1f918752fcfff3b9a
root <yangbofun@163.com>
2024-04-01 04:25:12 +0000

    lint

--- deeplink_ext/internlm_ops/__init__.py
+++ deeplink_ext/internlm_ops/__init__.py
@@ -5,8 +5,10 @@ from . import mha, rotary
 try:
     from .rms_norm import RMSNorm, RMSNormWithNormalizedShape
 except:
-    from .rms_norm_fallback import RMSNorm as RMSNorm, RMSNorm as RMSNormWithNormalizedShape
+    from .rms_norm_fallback import (
+        RMSNorm as RMSNorm,
+        RMSNorm as RMSNormWithNormalizedShape,
+    )
 
 
-__all__ =["mha","rotary", RMSNorm, RMSNormWithNormalizedShape]
-
+__all__ = ["mha", "rotary", RMSNorm, RMSNormWithNormalizedShape]
