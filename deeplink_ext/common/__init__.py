ac5a78e5844b30a7137fd79b09cc43795ef4399c
Zhangzefeng <zhang_zefeng@foxmail.com>
2024-04-02 16:06:29 +0800

    Update __init__.py

--- deeplink_ext/common/__init__.py
+++ deeplink_ext/common/__init__.py
@@ -1,4 +1,10 @@
 from .rms_norm import rms_norm_out, rms_norm, rms_norm_backward_out, rms_norm_backward
 
 
-__all__ = ["rms_norm_out", "rms_norm", "rms_norm_backward_out", "rms_norm_backward", "RMSNorm"]
+__all__ = [
+  "rms_norm_out",
+  "rms_norm",
+  "rms_norm_backward_out",
+  "rms_norm_backward",
+  "RMSNorm"，
+]
