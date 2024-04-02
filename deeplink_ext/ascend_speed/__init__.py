f55ac873591327bb353064bb916ebfee99686bff
YPOI-WX <wangxing2@sensetime.com>
2024-04-02 15:47:28 +0800

    add for flash attention

--- deeplink_ext/ascend_speed/__init__.py
+++ deeplink_ext/ascend_speed/__init__.py
@@ -2,5 +2,6 @@ from .rotary_embedding import apply_rotary, RotaryEmbedding
 from .adamw import adamw
 from .scaled_masked_softmax import ScaledMaskedSoftmax
 from .rms_norm import RMSNorm
+from .flash_attention import FlashSelfAttention
 
-__all__ = ["apply_rotary", "RotaryEmbedding", "adamw", "ScaledMaskedSoftmax", "RMSNorm"]
+__all__ = ["apply_rotary", "RotaryEmbedding", "adamw", "ScaledMaskedSoftmax", "RMSNorm", "FlashSelfAttention"]
