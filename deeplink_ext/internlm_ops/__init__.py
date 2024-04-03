a051f8a09bd72f50e9617ef22a566754aa35a7f6
YPOI-WX <wangxing2@sensetime.com>
2024-04-03 17:05:18 +0800

    temporarily skip fallback for internevo

--- deeplink_ext/internlm_ops/__init__.py
+++ deeplink_ext/internlm_ops/__init__.py
@@ -1,33 +1,33 @@
 # Copyright (c) 2024, DeepLink.
 
-
-_not_impl = "[deeplink_ext] {op_name} is not implemented in diopi. Falling back to the slower torch implementation."
-
-
-try:
-    from .rms_norm import RMSNorm, RMSNormWithNormalizedShape
-except:
-    print(
-        _not_impl.format(op_name="RMSNorm or RMSNormWithNormalizedShape"),
-    )
-    from .rms_norm_fallback import (
-        RMSNorm,
-        RMSNormWithNormalizedShape,
-    )
+# TODO: add fallback for the newest internevo
+# _not_impl = "[deeplink_ext] {op_name} is not implemented in diopi. Falling back to the slower torch implementation."
 
 
-try:
-    from .rotary_embedding import apply_rotary
-except:
-    print(_not_impl.format(op_name="apply_rotary"))
-    from .rotary_embedding_fallback import apply_rotary
+# try:
+#     from .rms_norm import RMSNorm, RMSNormWithNormalizedShape
+# except:
+#     print(
+#         _not_impl.format(op_name="RMSNorm or RMSNormWithNormalizedShape"),
+#     )
+#     from .rms_norm_fallback import (
+#         RMSNorm,
+#         RMSNormWithNormalizedShape,
+#     )
 
 
-try:
-    from .mha import SelfAttention, CrossAttention
-except Exception as e:
-    print(_not_impl.format(op_name="mha"))
-    from .mha_fallback import SelfAttention, CrossAttention
+# try:
+#     from .rotary_embedding import apply_rotary
+# except:
+#     print(_not_impl.format(op_name="apply_rotary"))
+#     from .rotary_embedding_fallback import apply_rotary
+
+
+# try:
+#     from .mha import SelfAttention, CrossAttention
+# except Exception as e:
+#     print(_not_impl.format(op_name="mha"))
+#     from .mha_fallback import SelfAttention, CrossAttention
 
 __all__ = [
     "DeepLinkSelfAttention",
