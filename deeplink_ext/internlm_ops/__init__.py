dcab7022418fd173c29d204999b5507f51c819f6
YPOI-WX <wangxing2@sensetime.com>
2024-04-05 18:32:49 +0800

    add fallback for flash attention when the input seqlen is padded

--- deeplink_ext/internlm_ops/__init__.py
+++ deeplink_ext/internlm_ops/__init__.py
@@ -1,33 +1,35 @@
 # Copyright (c) 2024, DeepLink.
 
 # TODO: add fallback for the newest internevo
-# _not_impl = "[deeplink_ext] {op_name} is not implemented in diopi. Falling back to the slower torch implementation."
+_not_impl = "[deeplink_ext] {op_name} is not implemented in diopi. Falling back to the slower torch implementation."
 
 
-# try:
-#     from .rms_norm import RMSNorm, RMSNormWithNormalizedShape
-# except:
-#     print(
-#         _not_impl.format(op_name="RMSNorm or RMSNormWithNormalizedShape"),
-#     )
-#     from .rms_norm_fallback import (
-#         RMSNorm,
-#         RMSNormWithNormalizedShape,
-#     )
+try:
+    from .flash_attention import DeepLinkSelfAttention, DeepLinkCrossAttention
+except Exception as e:
+    print(_not_impl.format(op_name="flash attention"))
+    from .flash_attention_fallback import SelfAttention as DeepLinkSelfAttention
+    from .flash_attention_fallback import CrossAttention as DeepLinkCrossAttention
 
 
-# try:
-#     from .rotary_embedding import apply_rotary
-# except:
-#     print(_not_impl.format(op_name="apply_rotary"))
-#     from .rotary_embedding_fallback import apply_rotary
+try:
+    from .rms_norm import DeepLinkMixedFusedRMSNorm
+except:
+    print(
+        _not_impl.format(op_name="RMSNorm"),
+    )
+    from .rms_norm_fallback import MixedRMSNormTorch as DeepLinkMixedFusedRMSNorm
 
 
-# try:
-#     from .mha import SelfAttention, CrossAttention
-# except Exception as e:
-#     print(_not_impl.format(op_name="mha"))
-#     from .mha_fallback import SelfAttention, CrossAttention
+try:
+    from .rotary_embedding import DeeplinkApplyRotaryEmb, DeeplinkApplyRotaryEmbQKV_
+except:
+    print(_not_impl.format(op_name="rotary embedding"))
+    from .rotary_embedding_fallback import ApplyRotaryEmb as DeeplinkApplyRotaryEmb
+    from .rotary_embedding_fallback import (
+        ApplyRotaryEmbQKV_ as DeeplinkApplyRotaryEmbQKV_,
+    )
+
 
 __all__ = [
     "adamw_for_internlm",
