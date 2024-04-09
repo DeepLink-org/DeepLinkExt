3a514266bc21febade62379cb206014476370e8a
YPOI-WX <wangxing2@sensetime.com>
2024-04-09 14:53:35 +0800

    move to corresponding folder

---
+++ tests/internevo/test_rotary_embedding.py
@@ -0,0 +1,73 @@
+# Copyright (c) 2024, DeepLink.
+
+import torch
+
+from deeplink_ext.internevo_ops.rotary_embedding import (
+    ApplyRotaryEmb,
+    ApplyRotaryEmbQKV_,
+)
+from deeplink_ext.internevo_ops.rotary_embedding_fallback import (
+    ApplyRotaryEmbTorch,
+    ApplyRotaryEmbQKV_Torch,
+)
+
+
+def _run_rotary_embedding(
+    rotary_embedding_module: type,
+    input: torch.Tensor,
+    cos: torch.Tensor,
+    sin: torch.Tensor,
+    interleaved: bool = False,
+):
+    model = rotary_embedding_module()
+    # model = model.cuda()
+    output = model(input, cos, sin, interleaved)
+    output.backward(torch.ones_like(output))
+    return output, input.grad
+
+
+def test_multi_cases_for_rotary_embedding():
+    input_dtype_list = [torch.float16, torch.bfloat16, torch.float32]
+    interleaved = False
+    for input_dtype in input_dtype_list:
+        input = torch.rand(
+            1, 64, 32, 64, dtype=input_dtype, device="cuda", requires_grad=True
+        )
+        cos = torch.rand(64, 32, dtype=input_dtype, device="cuda")
+        sin = torch.rand(64, 32, dtype=input_dtype, device="cuda")
+
+        output_ref, grad_ref = _run_rotary_embedding(
+            ApplyRotaryEmbTorch, input, cos, sin, interleaved
+        )
+        output_ext, grad_ext = _run_rotary_embedding(
+            ApplyRotaryEmb, input, cos, sin, interleaved
+        )
+        try:
+            assert torch.allclose(output_ref, output_ext)
+        except AssertionError:
+            print(
+                f"When input dtype is {input_dtype}, ApplyRotaryEmb fails to pass the forward test!"
+            )
+        else:
+            print(
+                f"When input dtype is {input_dtype}, ApplyRotaryEmb passes the forward test!"
+            )
+
+        try:
+            assert torch.allclose(grad_ref, grad_ext)
+        except AssertionError:
+            print(
+                f"When input dtype is {input_dtype}, ApplyRotaryEmb fails to pass the backward test!"
+            )
+        else:
+            print(
+                f"When input dtype is {input_dtype}, ApplyRotaryEmb passes the backward test!"
+            )
+
+
+def test_multi_cases_for_rotary_embedding_qkv_():
+    print("1")
+
+
+test_multi_cases_for_rotary_embedding()
+test_multi_cases_for_rotary_embedding_qkv_()
