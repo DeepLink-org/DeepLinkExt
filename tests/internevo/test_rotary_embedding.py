4948966262d30aa1da4e913927225da6a31cf051
yangbo <yangbofun@163.com>
2024-04-19 17:34:28 +0800

    feat: add torch.autograd.function test module (#99)
    * add torch.autograd.function test module

--- tests/internevo/test_rotary_embedding.py
+++ tests/internevo/test_rotary_embedding.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2024, DeepLink.
 
 import torch
+from tests.core import call_module, call_func, allclose
 
 from deeplink_ext.internevo_ops.rotary_embedding import (
     ApplyRotaryEmb,
@@ -12,51 +13,7 @@ from deeplink_ext.internevo_ops.rotary_embedding_fallback import (
 )
 
 
-class _TestRotaryEmbedding(torch.nn.Module):
-    def __init__(self, rotary_embedding_module):
-        super(_TestRotaryEmbedding, self).__init__()
-        self.rotary_embedding_module = rotary_embedding_module
-
-    def forward(self, input, cos, sin, interleaved):
-        return self.rotary_embedding_module.apply(
-            input,
-            cos,
-            sin,
-            interleaved,
-        )
-
-
-class _TestRotaryEmbeddingQKV_(torch.nn.Module):
-    def __init__(self, rotary_embedding_module):
-        super(_TestRotaryEmbeddingQKV_, self).__init__()
-        self.rotary_embedding_module = rotary_embedding_module
-
-    def forward(self, qkv, cos, sin, cos_k=None, sin_k=None, interleaved=False):
-        return self.rotary_embedding_module.apply(
-            qkv,
-            cos,
-            sin,
-            cos_k,
-            sin_k,
-            interleaved,
-        )
-
-
-def _run_rotary_embedding(
-    rotary_embedding_module: type,
-    input: torch.Tensor,
-    cos: torch.Tensor,
-    sin: torch.Tensor,
-    interleaved: bool = False,
-):
-
-    model = _TestRotaryEmbedding(rotary_embedding_module)
-    output = model(input, cos, sin, interleaved)
-    output.backward(torch.ones_like(output))
-    return output, input.grad
-
-
-def test_multi_cases_for_rotary_embedding():
+def test_ApplyRotaryEmb():
     input_dtype_list = [torch.float16, torch.bfloat16, torch.float32]
     interleaved = False
     for input_dtype in input_dtype_list:
@@ -66,38 +23,21 @@ def test_multi_cases_for_rotary_embedding():
         cos = torch.randn(64, 32, dtype=input_dtype, device="cuda")
         sin = torch.randn(64, 32, dtype=input_dtype, device="cuda")
 
-        output_ref, grad_ref = _run_rotary_embedding(
-            ApplyRotaryEmbTorch, input, cos, sin, interleaved
+        output_ref, grad_ref = call_func(
+            ApplyRotaryEmbTorch, "cuda", input_dtype, input, cos, sin, interleaved
         )
-        output_ext, grad_ext = _run_rotary_embedding(
-            ApplyRotaryEmb, input, cos, sin, interleaved
+        output, grad = call_func(
+            ApplyRotaryEmb, "cuda", input_dtype, input, cos, sin, interleaved
         )
-
-        assert torch.allclose(
-            output_ref, output_ext, rtol=1e-2, atol=1e-2
+        assert allclose(
+            output_ref, output, rtol=1e-2, atol=1e-2
         ), f"When input dtype is {input_dtype}, ApplyRotaryEmb fails to pass the forward test!"
-
-        assert torch.allclose(
-            grad_ref, grad_ext
+        assert allclose(
+            grad_ref, grad
         ), f"When input dtype is {input_dtype}, ApplyRotaryEmb fails to pass the backward test!"
-    print("Pass all forward and backward test cases of ApplyRotaryEmb successfully!")
 
 
-def _run_rotary_embedding_qkv_(
-    rotary_embedding_module: type,
-    qkv: torch.Tensor,
-    cos: torch.Tensor,
-    sin: torch.Tensor,
-    interleaved: bool = False,
-):
-
-    model = _TestRotaryEmbeddingQKV_(rotary_embedding_module)
-    output = model(qkv, cos, sin, interleaved=interleaved)
-    output.backward(torch.ones_like(output))
-    return output, qkv.grad
-
-
-def test_multi_cases_for_rotary_embedding_qkv_():
+def test_ApplyRotaryEmbQKV__qkv():
     # Note: For ascend, when dtype of input is fp32, the difference in calculation results is significant.
     input_dtype_list = [torch.float16, torch.bfloat16]
     interleaved = False
@@ -109,20 +49,33 @@ def test_multi_cases_for_rotary_embedding_qkv_():
         cos = torch.randn(64, 32, dtype=input_dtype, device="cuda")
         sin = torch.randn(64, 32, dtype=input_dtype, device="cuda")
 
-        output_ref, grad_ref = _run_rotary_embedding_qkv_(
-            ApplyRotaryEmbQKV_Torch, input_ref, cos, sin, interleaved
+        output_ref, grad_ref = call_func(
+            ApplyRotaryEmbQKV_Torch,
+            "cuda",
+            input_dtype,
+            input_ref,
+            cos,
+            sin,
+            None,
+            None,
+            interleaved,
         )
-        output_ext, grad_ext = _run_rotary_embedding_qkv_(
-            ApplyRotaryEmbQKV_, input_ext, cos, sin, interleaved
+        output_ext, grad_ext = call_func(
+            ApplyRotaryEmbQKV_,
+            "cuda",
+            input_dtype,
+            input_ext,
+            cos,
+            sin,
+            None,
+            None,
+            interleaved,
         )
 
-        assert torch.allclose(
+        assert allclose(
             output_ref, output_ext, rtol=1e-2, atol=1e-2
         ), f"When input dtype is {input_dtype}, ApplyRotaryEmbQKV_ fails to pass the forward test!"
 
-        assert torch.allclose(
+        assert allclose(
             grad_ref, grad_ext
         ), f"When input dtype is {input_dtype}, ApplyRotaryEmbQKV_ fails to pass the backward test!"
-    print(
-        "Pass all forward and backward test cases of ApplyRotaryEmbQKV_ successfully!"
-    )
