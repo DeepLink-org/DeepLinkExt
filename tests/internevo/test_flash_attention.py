3da7e168830bd0f61655af97d3f7b753e56e5fba
wangxing <131418410+POI-WX@users.noreply.github.com>
2024-04-19 10:40:34 +0800

    fix: modify fallback of flash attention (#96)
    * Support gqa for the fallback of flash attention.
    * Add the corresponding test cases.
    *  CI目前的7.0 软件栈无法通过使用casual mask测试，本地8.0软件栈可以测试通过。

--- tests/internevo/test_flash_attention.py
+++ tests/internevo/test_flash_attention.py
@@ -13,23 +13,59 @@ from deeplink_ext.internevo_ops.flash_attention_fallback import (
 )
 
 
-def test_self_attention():
-    batch, seqlen, nheads, headdim = [8, 32, 16, 64]
+def test_self_attention_qkv_mha():
+    batch, seqlen, num_heads, headdim = [8, 32, 32, 64]
+
+    qkv_gpu = torch.rand(
+        [batch, seqlen, 3, num_heads, headdim],
+        dtype=torch.float16,
+        requires_grad=True,
+        device="cuda",
+    )
+
+    qkv_cpu = copy_to_cpu(
+        [
+            qkv_gpu,
+        ]
+    )
+    ouput_forward_cpu, grads_cpu = call_module(
+        SelfAttention(),
+        qkv_cpu[0],
+        None,
+        None,
+        None,
+        None,
+    )
+    ouput_forward_gpu, grads_gpu = call_module(
+        FlashSelfAttention().cuda(),
+        qkv_gpu,
+        None,
+        None,
+        None,
+        None,
+    )
+    assert allclose(ouput_forward_cpu, ouput_forward_gpu, rtol=1e-3, atol=1e-3)
+    assert allclose(grads_cpu, grads_gpu, rtol=1e-3, atol=1e-3)
+
+
+def test_self_attention_q_k_v_gqa():
+    batch, seqlen, num_q_heads, headdim = [8, 32, 32, 64]
+    num_kv_heads = 8
 
     q_gpu = torch.rand(
-        [batch, seqlen, nheads, headdim],
+        [batch, seqlen, num_q_heads, headdim],
         dtype=torch.float16,
         requires_grad=True,
         device="cuda",
     )
     k_gpu = torch.rand(
-        [batch, seqlen, nheads, headdim],
+        [batch, seqlen, num_kv_heads, headdim],
         dtype=torch.float16,
         requires_grad=True,
         device="cuda",
     )
     v_gpu = torch.rand(
-        [batch, seqlen, nheads, headdim],
+        [batch, seqlen, num_kv_heads, headdim],
         dtype=torch.float16,
         requires_grad=True,
         device="cuda",
@@ -37,26 +73,106 @@ def test_self_attention():
 
     q_cpu, k_cpu, v_cpu = copy_to_cpu([q_gpu, k_gpu, v_gpu])
     ouput_forward_cpu, grads_cpu = call_module(
-        SelfAttention(), None, q_cpu, k_cpu, v_cpu, None
+        SelfAttention(),
+        None,
+        q_cpu,
+        k_cpu,
+        v_cpu,
+        None,
     )
     ouput_forward_gpu, grads_gpu = call_module(
-        FlashSelfAttention().cuda(), None, q_gpu, k_gpu, v_gpu, None
+        FlashSelfAttention().cuda(),
+        None,
+        q_gpu,
+        k_gpu,
+        v_gpu,
+        None,
     )
     assert allclose(ouput_forward_cpu, ouput_forward_gpu, rtol=1e-3, atol=1e-3)
     assert allclose(grads_cpu, grads_gpu, rtol=1e-3, atol=1e-3)
 
 
-def test_cross_attention():
-    batch, seqlen, nheads, headdim = [8, 32, 16, 64]
+def test_self_attention_q_kv_gqa():
+    batch, seqlen, num_q_heads, headdim = [8, 32, 32, 64]
+    num_kv_heads = 8
 
     q_gpu = torch.rand(
-        [batch, seqlen, nheads, headdim],
+        [batch, seqlen, num_q_heads, headdim],
         dtype=torch.float16,
         requires_grad=True,
         device="cuda",
     )
     kv_gpu = torch.rand(
-        [batch, seqlen, 2, nheads, headdim],
+        [batch, seqlen, 2, num_kv_heads, headdim],
+        dtype=torch.float16,
+        requires_grad=True,
+        device="cuda",
+    )
+
+    q_cpu, kv_cpu = copy_to_cpu([q_gpu, kv_gpu])
+    ouput_forward_cpu, grads_cpu = call_module(
+        SelfAttention(),
+        None,
+        q_cpu,
+        None,
+        None,
+        kv_cpu,
+    )
+    ouput_forward_gpu, grads_gpu = call_module(
+        FlashSelfAttention().cuda(),
+        None,
+        q_gpu,
+        None,
+        None,
+        kv_gpu,
+    )
+    assert allclose(ouput_forward_cpu, ouput_forward_gpu, rtol=1e-3, atol=1e-3)
+    assert allclose(grads_cpu, grads_gpu, rtol=1e-3, atol=1e-3)
+
+
+def test_cross_attention_q_kv_mha():
+    batch, seqlen, num_heads, headdim = [8, 32, 32, 64]
+
+    q_gpu = torch.rand(
+        [batch, seqlen, num_heads, headdim],
+        dtype=torch.float16,
+        requires_grad=True,
+        device="cuda",
+    )
+    kv_gpu = torch.rand(
+        [batch, seqlen, 2, num_heads, headdim],
+        dtype=torch.float16,
+        requires_grad=True,
+        device="cuda",
+    )
+
+    q_cpu, kv_cpu = copy_to_cpu([q_gpu, kv_gpu])
+    ouput_forward_cpu, grads_cpu = call_module(
+        CrossAttention(),
+        q_cpu,
+        kv_cpu,
+    )
+    ouput_forward_gpu, grads_gpu = call_module(
+        FlashCrossAttention().cuda(),
+        q_gpu,
+        kv_gpu,
+    )
+
+    assert allclose(ouput_forward_cpu, ouput_forward_gpu, rtol=1e-3, atol=1e-3)
+    assert allclose(grads_cpu, grads_gpu, rtol=1e-3, atol=1e-3)
+
+
+def test_cross_attention_q_kv_gqa():
+    batch, seqlen, num_q_heads, headdim = [8, 32, 32, 64]
+    num_kv_heads = 8
+    q_gpu = torch.rand(
+        [batch, seqlen, num_q_heads, headdim],
+        dtype=torch.float16,
+        requires_grad=True,
+        device="cuda",
+    )
+    kv_gpu = torch.rand(
+        [batch, seqlen, 2, num_kv_heads, headdim],
         dtype=torch.float16,
         requires_grad=True,
         device="cuda",
@@ -65,7 +181,9 @@ def test_cross_attention():
     q_cpu, kv_cpu = copy_to_cpu([q_gpu, kv_gpu])
     ouput_forward_cpu, grads_cpu = call_module(CrossAttention(), q_cpu, kv_cpu)
     ouput_forward_gpu, grads_gpu = call_module(
-        FlashCrossAttention().cuda(), q_gpu, kv_gpu
+        FlashCrossAttention().cuda(),
+        q_gpu,
+        kv_gpu,
     )
 
     assert allclose(ouput_forward_cpu, ouput_forward_gpu, rtol=1e-3, atol=1e-3)
