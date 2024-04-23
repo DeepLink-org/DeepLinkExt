33bbb614efafb8f292f73ec9b3b847a653f4c1a7
wangxing <131418410+POI-WX@users.noreply.github.com>
2024-04-23 18:05:46 +0800

    fix: fix rms norm (#102)
    Fix rms norm on the wrong shape of inv_rms tensor.
    
    ---------
    
    Co-authored-by: root <zsksmhq>

--- deeplink_ext/ascend_speed/rms_norm.py
+++ deeplink_ext/ascend_speed/rms_norm.py
@@ -19,8 +19,9 @@ class RMSNorm(torch.autograd.Function):
             if input_dtype in [torch.bfloat16, torch.float16]
             else input_dtype
         )
+        n = weight.dim()
         inv_rms = torch.empty(
-            list(hidden_states.shape[:-1]) + [1],
+            list(hidden_states.shape[:-n]) + [1] * n,
             dtype=acc_dtype,
             device=hidden_states.device,
         )
--- deeplink_ext/internevo_ops/rms_norm.py
+++ deeplink_ext/internevo_ops/rms_norm.py
@@ -28,8 +28,9 @@ class _MixedFusedRMSNormFunction(torch.autograd.Function):
             if input_dtype in [torch.bfloat16, torch.float16]
             else input_dtype
         )
+        n = len(normalized_shape)
         inv_rms = torch.empty(
-            list(hidden_states.shape[:-1]) + [1],
+            list(hidden_states.shape[:-n]) + [1] * n,
             dtype=acc_dtype,
             device=hidden_states.device,
         )
