cc7a917284e47ee4866292e8f58976c738d926d1
root <zsksmhq>
2024-04-23 17:34:37 +0800

    fix rms norm

--- deeplink_ext/ascend_speed/rms_norm.py
+++ deeplink_ext/ascend_speed/rms_norm.py
@@ -19,8 +19,9 @@ class RMSNorm(torch.autograd.Function):
             if input_dtype in [torch.bfloat16, torch.float16]
             else input_dtype
         )
+        N = weight.dim()
         inv_rms = torch.empty(
-            list(hidden_states.shape[:-1]) + [1],
+            list(hidden_states.shape[:-N]) + [1] * N,
             dtype=acc_dtype,
             device=hidden_states.device,
         )
--- deeplink_ext/internevo_ops/rms_norm.py
+++ deeplink_ext/internevo_ops/rms_norm.py
@@ -28,8 +28,9 @@ class _MixedFusedRMSNormFunction(torch.autograd.Function):
             if input_dtype in [torch.bfloat16, torch.float16]
             else input_dtype
         )
+        N = len(normalized_shape)
         inv_rms = torch.empty(
-            list(hidden_states.shape[:-1]) + [1],
+            list(hidden_states.shape[:-N]) + [1] * N,
             dtype=acc_dtype,
             device=hidden_states.device,
         )
