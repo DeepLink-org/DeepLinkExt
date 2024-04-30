4f425c0ef1edc44131880ad98e36e9da12cd82cd
POI-WX <wangxing2@sensetime.com>
2024-04-30 09:37:17 +0800

    add mha test case

--- tests/internevo/test_varlen_flash_attention.py
+++ tests/internevo/test_varlen_flash_attention.py
@@ -124,6 +124,70 @@ def test_self_attention_varlen_q_k_v_gqa():
     assert allclose(grads_ref, grads_ext, rtol=1e-5, atol=5e-2)
 
 
+def test_self_attention_varlen_q_kv_mha():
+    total_seqlen, num_heads, headdim = [16384, 6, 64]
+
+    q_ref = torch.randn(
+        [total_seqlen, num_heads, headdim],
+        dtype=torch.float16,
+        requires_grad=True,
+        device="cuda",
+    )
+    kv_ref = torch.randn(
+        [total_seqlen, 2, num_heads, headdim],
+        dtype=torch.float16,
+        requires_grad=True,
+        device="cuda",
+    )
+    q_ext = q_ref.clone().detach().requires_grad_(True)
+    kv_ext = kv_ref.clone().detach().requires_grad_(True)
+
+    cu_seqlens_q_ref = torch.tensor([  0,   186,   382,  1259,  1464,  2547,  2705,  3495,  3854,  4696,
+         4762,  4885,  5118,  5355,  5503,  5760,  6168,  6353,  8272,  8461,
+         9273,  9531,  9763,  9871, 10234, 10370, 10574, 10712, 11022, 11236,
+        11599, 11837, 12179, 12320, 12560, 12731, 13038, 13180, 13477, 14025,
+        14742, 14872, 15131, 15773, 15967, 16110, 16384], dtype=torch.int64, device='cuda')
+    cu_seqlens_k_ref = torch.tensor([  0,   186,   382,  1259,  1464,  2547,  2705,  3495,  3854,  4696,
+         4762,  4885,  5118,  5355,  5503,  5760,  6168,  6353,  8272,  8461,
+         9273,  9531,  9763,  9871, 10234, 10370, 10574, 10712, 11022, 11236,
+        11599, 11837, 12179, 12320, 12560, 12731, 13038, 13180, 13477, 14025,
+        14742, 14872, 15131, 15773, 15967, 16110, 16384], dtype=torch.int64, device='cuda')
+    max_seqlen = 1919
+
+    ouput_forward_ref, grads_ref = call_module(
+        SelfAttention().cuda(),
+        None,
+        q_ref,
+        None,
+        None,
+        kv_ref,
+        True,
+        None,
+        None,
+        cu_seqlens_q_ref,
+        cu_seqlens_k_ref,
+        max_seqlen,
+        max_seqlen,
+    )
+    ouput_forward_ext, grads_ext = call_module(
+        FlashSelfAttention().cuda(),
+        None,
+        q_ext,
+        None,
+        None,
+        kv_ext,
+        True,
+        None,
+        None,
+        cu_seqlens_q_ref,
+        cu_seqlens_k_ref,
+        max_seqlen,
+        max_seqlen,
+    )
+    assert allclose(ouput_forward_ref, ouput_forward_ext, rtol=1e-5, atol=1e-5)
+    assert allclose(grads_ref, grads_ext, rtol=1e-5, atol=5e-2)
+
+
 def test_self_attention_varlen_q_kv_gqa():
     total_seqlen, num_q_heads, headdim = [16384, 6, 64]
     num_kv_heads = 2
