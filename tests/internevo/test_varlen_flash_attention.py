b81ca1fd5835963b941e23061429e224f16eeab3
hellozmz <40719054@qq.com>
2024-06-21 11:03:32 +0800

    fix: change cu_seqlens_ref int32 to int64

--- tests/internevo/test_varlen_flash_attention.py
+++ tests/internevo/test_varlen_flash_attention.py
@@ -34,7 +34,7 @@ class TestFlashSelfAttention:
         qkv_ext = qkv_ref.clone().detach().requires_grad_(True)
 
         cu_seqlens_ref = torch.tensor(
-            [0, 32, 64, 128, 256], dtype=torch.int64, device="cuda"
+            [0, 32, 64, 128, 256], dtype=torch.int32, device="cuda"
         )
         max_seqlen = 128
 
@@ -91,10 +91,10 @@ class TestFlashSelfAttention:
         v_ext = v_ref.clone().detach().requires_grad_(True)
 
         cu_seqlens_q_ref = torch.tensor(
-            [0, 32, 64, 128, 256], dtype=torch.int64, device="cuda"
+            [0, 32, 64, 128, 256], dtype=torch.int32, device="cuda"
         )
         cu_seqlens_k_ref = torch.tensor(
-            [0, 32, 64, 128, 256], dtype=torch.int64, device="cuda"
+            [0, 32, 64, 128, 256], dtype=torch.int32, device="cuda"
         )
         max_seqlen = 128
 
@@ -150,8 +150,8 @@ class TestFlashSelfAttention:
         q_ext = q_ref.clone().detach().requires_grad_(True)
         kv_ext = kv_ref.clone().detach().requires_grad_(True)
 
-        cu_seqlens_q_ref = torch.tensor(g_cu_seqlens, dtype=torch.int64, device="cuda")
-        cu_seqlens_k_ref = torch.tensor(g_cu_seqlens, dtype=torch.int64, device="cuda")
+        cu_seqlens_q_ref = torch.tensor(g_cu_seqlens, dtype=torch.int32, device="cuda")
+        cu_seqlens_k_ref = torch.tensor(g_cu_seqlens, dtype=torch.int32, device="cuda")
         max_seqlen = 1919
 
         ouput_forward_ref, grads_ref = call_module(
@@ -207,8 +207,8 @@ class TestFlashSelfAttention:
         q_ext = q_ref.clone().detach().requires_grad_(True)
         kv_ext = kv_ref.clone().detach().requires_grad_(True)
 
-        cu_seqlens_q_ref = torch.tensor(g_cu_seqlens, dtype=torch.int64, device="cuda")
-        cu_seqlens_k_ref = torch.tensor(g_cu_seqlens, dtype=torch.int64, device="cuda")
+        cu_seqlens_q_ref = torch.tensor(g_cu_seqlens, dtype=torch.int32, device="cuda")
+        cu_seqlens_k_ref = torch.tensor(g_cu_seqlens, dtype=torch.int32, device="cuda")
         max_seqlen = 1919
 
         ouput_forward_ref, grads_ref = call_module(
@@ -267,15 +267,15 @@ class TestFlashSelfAttention:
 
         # fmt: off
         # the new sequence lengths for the test case, latest sequence length is 20206-16110=4096
-        new_g_cu_seqlens = [
+        cu_seqlens_max_length_4096 = [
             0, 186, 382, 1259, 1464, 2547, 2705, 3495, 3854, 4696, 4762, 4885, 5118, 5355, 5503, 5760, 6168, 6353,
             8272, 8461, 9273, 9531, 9763, 9871, 10234, 10370, 10574, 10712, 11022, 11236, 11599, 11837, 12179, 12320,
             12560, 12731, 13038, 13180, 13477, 14025, 14742, 14872, 15131, 15773, 15967, 16110, 20206,
         ]
         # fmt: on
 
-        cu_seqlens_q_ref = torch.tensor(new_g_cu_seqlens, dtype=torch.int64, device="cuda")
-        cu_seqlens_k_ref = torch.tensor(new_g_cu_seqlens, dtype=torch.int64, device="cuda")
+        cu_seqlens_q_ref = torch.tensor(cu_seqlens_max_length_4096, dtype=torch.int32, device="cuda")
+        cu_seqlens_k_ref = torch.tensor(cu_seqlens_max_length_4096, dtype=torch.int32, device="cuda")
         # the maximum sequence length is 4096
         max_seqlen = 4096
 
@@ -310,6 +310,7 @@ class TestFlashSelfAttention:
             max_seqlen,
         )
         assert allclose(ouput_forward_ref, ouput_forward_ext, rtol=1e-5, atol=1e-5)
+        assert allclose(grads_ref, grads_ext, rtol=1e-5, atol=1e-2)
 
 class TestFlashCrossAttention:
     def test_cross_attention_varlen_q_kv_mha(self):
@@ -330,8 +331,8 @@ class TestFlashCrossAttention:
         q_ext = q_ref.clone().detach().requires_grad_(True)
         kv_ext = kv_ref.clone().detach().requires_grad_(True)
 
-        cu_seqlens_ref = torch.tensor(g_cu_seqlens, dtype=torch.int64, device="cuda")
-        cu_seqlens_k_ref = torch.tensor(g_cu_seqlens, dtype=torch.int64, device="cuda")
+        cu_seqlens_ref = torch.tensor(g_cu_seqlens, dtype=torch.int32, device="cuda")
+        cu_seqlens_k_ref = torch.tensor(g_cu_seqlens, dtype=torch.int32, device="cuda")
         max_seqlen = 1919
 
         ouput_forward_ref, grads_ref = call_module(
@@ -378,10 +379,10 @@ class TestFlashCrossAttention:
         kv_ext = kv_ref.clone().detach().requires_grad_(True)
 
         cu_seqlens_ref = torch.tensor(
-            [0, 32, 64, 128, 256], dtype=torch.int64, device="cuda"
+            [0, 32, 64, 128, 256], dtype=torch.int32, device="cuda"
         )
         cu_seqlens_k_ref = torch.tensor(
-            [0, 32, 64, 128, 256], dtype=torch.int64, device="cuda"
+            [0, 32, 64, 128, 256], dtype=torch.int32, device="cuda"
         )
         max_seqlen = 128
 
@@ -431,10 +432,10 @@ class TestFlashCrossAttention:
 
         # last sequence length is 4224-128=4096
         cu_seqlens_ref = torch.tensor(
-            [0, 32, 64, 128, 4224], dtype=torch.int64, device="cuda"
+            [0, 32, 64, 128, 4224], dtype=torch.int32, device="cuda"
         )
         cu_seqlens_k_ref = torch.tensor(
-            [0, 32, 64, 128, 4224], dtype=torch.int64, device="cuda"
+            [0, 32, 64, 128, 4224], dtype=torch.int32, device="cuda"
         )
         # the maximum sequence length is 4096
         max_seqlen = 4096
