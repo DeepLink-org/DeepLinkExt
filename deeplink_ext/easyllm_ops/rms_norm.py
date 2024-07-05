d0c51e7bd5dfa7dd6c4a1e879067a71b966fc633
POI-WX <wangxing2@sensetime.com>
2024-07-05 18:51:43 +0800

    share same code with ascend speed on rms norm op

--- deeplink_ext/easyllm_ops/rms_norm.py
+++ deeplink_ext/easyllm_ops/rms_norm.py
@@ -1,55 +1,8 @@
 # Copyright (c) 2024, DeepLink.
 
-import torch
-
-
-import deeplink_ext.cpp_extensions as ext
-
-assert hasattr(ext, "rms_norm") and hasattr(ext, "rms_norm_backward")
-
+from deeplink_ext.ascend_speed import RMSNorm
 
 __all__ = ["rms_norm"]
-
-
-class RMSNorm(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, hidden_states, weight, eps):
-        output = torch.empty_like(hidden_states)
-        input_dtype = hidden_states.dtype
-        acc_dtype = (
-            torch.float32
-            if input_dtype in [torch.bfloat16, torch.float16]
-            else input_dtype
-        )
-        n = weight.dim()
-        inv_rms = torch.empty(
-            list(hidden_states.shape[:-n]),
-            dtype=acc_dtype,
-            device=hidden_states.device,
-        )
-        ext.rms_norm(output, inv_rms, hidden_states, weight.shape, weight, None, eps)
-        ctx.save_for_backward(hidden_states, inv_rms, weight)
-        ctx.eps = eps
-        return output
-
-    @staticmethod
-    def backward(ctx, dout):
-        hidden_states, inv_rms, weight = ctx.saved_tensors
-        grad_input = torch.empty_like(hidden_states)
-        grad_weight = torch.empty_like(weight)
-        ext.rms_norm_backward(
-            grad_input,
-            grad_weight,
-            None,
-            dout,
-            hidden_states,
-            weight,
-            None,
-            inv_rms,
-            weight.shape,
-            ctx.eps,
-        )
-        return grad_input, grad_weight, None
 
 
 def rms_norm(x, weight, epsilon):
