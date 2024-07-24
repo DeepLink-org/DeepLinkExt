a871436fc2d4e2a70c5989319e8573002a879ed0
POI-WX <wangxing2@sensetime.com>
2024-07-24 13:51:27 +0800

    add test case for varlen flash attention

--- tests/internevo/test_flash_attention.py
+++ tests/internevo/test_flash_attention.py
@@ -48,42 +48,6 @@ def test_flash_attn_qkvpacked_func_mha():
     assert allclose(grads_cpu, grads_gpu, rtol=1e-3, atol=1e-3)
 
 
-def test_flash_attn_kvpacked_func_mha():
-    batch, seqlen, num_heads, headdim = [8, 32, 32, 64]
-
-    q_gpu = torch.rand(
-        [batch, seqlen, num_heads, headdim],
-        dtype=torch.float16,
-        requires_grad=True,
-        device="cuda",
-    )
-    kv_gpu = torch.rand(
-        [batch, seqlen, 2, num_heads, headdim],
-        dtype=torch.float16,
-        requires_grad=True,
-        device="cuda",
-    )
-
-    q_cpu, kv_cpu = copy_to_cpu([q_gpu, kv_gpu])
-    ouput_forward_cpu, grads_cpu = call_normal_func(
-        torch_attn_kvpacked_func,
-        q_cpu,
-        kv_cpu,
-        dropout_p=0.0,
-        causal=True,
-    )
-    ouput_forward_gpu, grads_gpu = call_normal_func(
-        flash_attn_kvpacked_func,
-        q_gpu,
-        kv_gpu,
-        dropout_p=0.0,
-        causal=True,
-    )
-
-    assert allclose(ouput_forward_cpu, ouput_forward_gpu, rtol=1e-3, atol=1e-3)
-    assert allclose(grads_cpu, grads_gpu, rtol=1e-3, atol=1e-3)
-
-
 def test_flash_attn_kvpacked_func_gqa():
     batch, seqlen, num_q_heads, headdim = [8, 32, 32, 64]
     num_kv_heads = 8
@@ -113,50 +77,6 @@ def test_flash_attn_kvpacked_func_gqa():
         flash_attn_kvpacked_func,
         q_gpu,
         kv_gpu,
-        dropout_p=0.0,
-        causal=True,
-    )
-
-    assert allclose(ouput_forward_cpu, ouput_forward_gpu, rtol=1e-3, atol=1e-3)
-    assert allclose(grads_cpu, grads_gpu, rtol=1e-3, atol=1e-3)
-
-
-def test_flash_attn_func_mha():
-    batch, seqlen, num_heads, headdim = [8, 32, 32, 64]
-
-    q_gpu = torch.rand(
-        [batch, seqlen, num_heads, headdim],
-        dtype=torch.float16,
-        requires_grad=True,
-        device="cuda",
-    )
-    k_gpu = torch.rand(
-        [batch, seqlen, num_heads, headdim],
-        dtype=torch.float16,
-        requires_grad=True,
-        device="cuda",
-    )
-    v_gpu = torch.rand(
-        [batch, seqlen, num_heads, headdim],
-        dtype=torch.float16,
-        requires_grad=True,
-        device="cuda",
-    )
-
-    q_cpu, k_cpu, v_cpu = copy_to_cpu([q_gpu, k_gpu, v_gpu])
-    ouput_forward_cpu, grads_cpu = call_normal_func(
-        torch_attn_func,
-        q_cpu,
-        k_cpu,
-        v_cpu,
-        dropout_p=0.0,
-        causal=True,
-    )
-    ouput_forward_gpu, grads_gpu = call_normal_func(
-        flash_attn_func,
-        q_gpu,
-        k_gpu,
-        v_gpu,
         dropout_p=0.0,
         causal=True,
     )
---
+++ tests/internevo/test_varlen_flash_attention.py
@@ -0,0 +1,171 @@
+# Copyright (c) 2024, DeepLink.
+
+import torch
+from tests.core import allclose, call_normal_func
+
+from deeplink_ext.internevo_ops.flash_attention_fallback import (
+    torch_attn_varlen_qkvpacked_func,
+    torch_attn_varlen_kvpacked_func,
+    torch_attn_varlen_func,
+)
+from deeplink_ext.internevo_ops.flash_attention import (
+    flash_attn_varlen_qkvpacked_func,
+    flash_attn_varlen_kvpacked_func,
+    flash_attn_varlen_func,
+)
+
+
+def test_flash_attn_varlen_qkvpacked_func_mha():
+    total_seqlen, num_heads, headdim = [256, 32, 64]
+
+    qkv_ref = torch.randn(
+        [total_seqlen, 3, num_heads, headdim],
+        dtype=torch.float16,
+        requires_grad=True,
+        device="cuda",
+    )
+    qkv_ext = qkv_ref.clone().detach().requires_grad_(True)
+
+    cu_seqlens_ref = torch.tensor(
+        [0, 32, 64, 128, 256], dtype=torch.int32, device="cuda"
+    )
+    max_seqlen = 128
+
+    ouput_forward_ref, grads_ref = call_normal_func(
+        torch_attn_varlen_qkvpacked_func,
+        qkv_ref,
+        cu_seqlens_ref,
+        max_seqlen,
+        dropout_p=0.0,
+        causal=True,
+    )
+    ouput_forward_ext, grads_ext = call_normal_func(
+        flash_attn_varlen_qkvpacked_func,
+        qkv_ext,
+        cu_seqlens_ref,
+        max_seqlen,
+        dropout_p=0.0,
+        causal=True,
+    )
+
+    assert allclose(ouput_forward_ref, ouput_forward_ext, rtol=1e-5, atol=1e-5)
+    assert allclose(grads_ref, grads_ext, rtol=1e-5, atol=1e-2)
+
+
+def test_flash_attn_varlen_kvpacked_func_gqa():
+    total_seqlen, num_q_heads, headdim = [256, 32, 64]
+    num_kv_heads = 8
+
+    q_ref = torch.randn(
+        [total_seqlen, num_q_heads, headdim],
+        dtype=torch.float16,
+        requires_grad=True,
+        device="cuda",
+    )
+    kv_ref = torch.randn(
+        [total_seqlen, 2, num_kv_heads, headdim],
+        dtype=torch.float16,
+        requires_grad=True,
+        device="cuda",
+    )
+    q_ext = q_ref.clone().detach().requires_grad_(True)
+    kv_ext = kv_ref.clone().detach().requires_grad_(True)
+
+    cu_seqlens_q_ref = torch.tensor(
+        [0, 32, 64, 128, 256], dtype=torch.int32, device="cuda"
+    )
+    cu_seqlens_k_ref = torch.tensor(
+        [0, 32, 64, 128, 256], dtype=torch.int32, device="cuda"
+    )
+    max_seqlen_q = 128
+    max_seqlen_k = 128
+
+    ouput_forward_ref, grads_ref = call_normal_func(
+        torch_attn_varlen_kvpacked_func,
+        q_ref,
+        kv_ref,
+        cu_seqlens_q_ref,
+        cu_seqlens_k_ref,
+        max_seqlen_q,
+        max_seqlen_k,
+        dropout_p=0.0,
+        causal=True,
+    )
+    ouput_forward_ext, grads_ext = call_normal_func(
+        flash_attn_varlen_kvpacked_func,
+        q_ext,
+        kv_ext,
+        cu_seqlens_q_ref,
+        cu_seqlens_k_ref,
+        max_seqlen_q,
+        max_seqlen_k,
+        dropout_p=0.0,
+        causal=True,
+    )
+
+    assert allclose(ouput_forward_ref, ouput_forward_ext, rtol=1e-5, atol=1e-5)
+    assert allclose(grads_ref, grads_ext, rtol=1e-3, atol=1e-2)
+
+
+def test_flash_attn_varlen_func_gqa():
+    total_seqlen, num_q_heads, headdim = [256, 32, 64]
+    num_kv_heads = 8
+
+    q_ref = torch.randn(
+        [total_seqlen, num_q_heads, headdim],
+        dtype=torch.float16,
+        requires_grad=True,
+        device="cuda",
+    )
+    k_ref = torch.randn(
+        [total_seqlen, num_kv_heads, headdim],
+        dtype=torch.float16,
+        requires_grad=True,
+        device="cuda",
+    )
+    v_ref = torch.randn(
+        [total_seqlen, num_kv_heads, headdim],
+        dtype=torch.float16,
+        requires_grad=True,
+        device="cuda",
+    )
+    q_ext = q_ref.clone().detach().requires_grad_(True)
+    k_ext = k_ref.clone().detach().requires_grad_(True)
+    v_ext = v_ref.clone().detach().requires_grad_(True)
+
+    cu_seqlens_q_ref = torch.tensor(
+        [0, 32, 64, 128, 256], dtype=torch.int32, device="cuda"
+    )
+    cu_seqlens_k_ref = torch.tensor(
+        [0, 32, 64, 128, 256], dtype=torch.int32, device="cuda"
+    )
+    max_seqlen_q = 128
+    max_seqlen_k = 128
+
+    ouput_forward_ref, grads_ref = call_normal_func(
+        torch_attn_varlen_func,
+        q_ref,
+        k_ref,
+        v_ref,
+        cu_seqlens_q_ref,
+        cu_seqlens_k_ref,
+        max_seqlen_q,
+        max_seqlen_k,
+        dropout_p=0.0,
+        causal=True,
+    )
+    ouput_forward_ext, grads_ext = call_normal_func(
+        flash_attn_varlen_func,
+        q_ext,
+        k_ext,
+        v_ext,
+        cu_seqlens_q_ref,
+        cu_seqlens_k_ref,
+        max_seqlen_q,
+        max_seqlen_k,
+        dropout_p=0.0,
+        causal=True,
+    )
+
+    assert allclose(ouput_forward_ref, ouput_forward_ext, rtol=1e-5, atol=1e-5)
+    assert allclose(grads_ref, grads_ext, rtol=1e-5, atol=1e-2)
