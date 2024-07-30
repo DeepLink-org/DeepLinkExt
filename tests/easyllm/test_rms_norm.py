f6f170e240c562053fc88817cceffa596600d067
POI-WX <wangxing2@sensetime.com>
2024-07-30 11:50:46 +0800

    update

--- tests/easyllm/test_rms_norm.py
+++ tests/easyllm/test_rms_norm.py
@@ -1,32 +1,12 @@
 # Copyright (c) 2024, DeepLink.
 
-import numbers
-
 import torch
-from tests.core import call_module, allclose, call_func
-from deeplink_ext.easyllm_ops.rms_norm import RMSNorm
+from tests.core import call_normal_func, allclose
+from deeplink_ext.easyllm_ops.rms_norm import rms_norm
+from deeplink_ext.easyllm_ops.rms_norm_fallback import rms_norm_torch
 
 
-class RMSNormTorch(torch.nn.Module):
-    """A custom PyTorch module for RMS normalization."""
-
-    def __init__(self, normalized_shape, eps=1e-5):
-        super().__init__()
-        if isinstance(normalized_shape, numbers.Integral):
-            normalized_shape = (normalized_shape,)
-        self.normalized_shape = torch.Size(normalized_shape)
-        self.weight = torch.nn.Parameter(torch.ones(normalized_shape))
-        self.eps = eps
-
-    def _norm(self, input, dims):
-        return input * torch.rsqrt(input.pow(2).mean(dims, keepdim=True) + self.eps)
-
-    def forward(self, input: torch.Tensor):
-        dims = tuple(i for i in range(-1, -len(self.normalized_shape) - 1, -1))
-        return self._norm(input.float(), dims).type_as(input) * self.weight
-
-
-def test_RMSNorm():
+def test_rms_norm():
     input_dtype_list = [torch.float16, torch.bfloat16]
     weight_dtype_list = [torch.float16, torch.bfloat16]
     for input_dtype, weight_dtype in zip(input_dtype_list, weight_dtype_list):
@@ -35,22 +15,28 @@ def test_RMSNorm():
         )
         hidden_states_ext = hidden_states_ref.clone().detach().requires_grad_(True)
 
-        weight = torch.nn.Parameter(
+        weight_ref = torch.nn.Parameter(
             torch.ones(
-                list(hidden_states_ext.shape)[-1], dtype=weight_dtype, device="cuda"
+                list(hidden_states_ref.shape)[-1], dtype=weight_dtype, device="cuda"
             ),
-            requires_grad=False,
+            requires_grad=True,
         )
+        weight_ext = weight_ref.clone().detach().requires_grad_(True)
 
-        output_ref, grad_ref = call_module(
-            RMSNormTorch(list(hidden_states_ref.shape)[-1], 1e-5)
-            .cuda()
-            .to(weight_dtype),
+        epsilon = 1e-5
+
+        output_ref, grad_ref = call_normal_func(
+            rms_norm_torch,
             hidden_states_ref,
+            weight_ref,
+            epsilon,
         )
 
-        output_ext, grad_ext = call_func(
-            RMSNorm, "cuda", weight_dtype, hidden_states_ext, weight, 1e-5
+        output_ext, grad_ext = call_normal_func(
+            rms_norm,
+            hidden_states_ext,
+            weight_ext,
+            epsilon,
         )
 
         assert allclose(
