22bffd4165115d80b2edf7c07edfcd6d694e093f
Fu Jingguo <fujingguo@sensetime.com>
2024-09-12 10:41:16 +0800

    feat: reimpl rotary embedding for npu (#127)
    1. reimpl rotary embedding with  npu_rotary_mul  in torch_npu
    2. using combined rms_norm and rotary_embedding temporarily for
    accuracy.

--- deeplink_ext/internevo_ops/rotary_embedding.py
+++ deeplink_ext/internevo_ops/rotary_embedding.py
@@ -4,7 +4,8 @@ from deeplink_ext.utils import PlatformType, deeplink_ext_get_platform_type
 
 platform_type = deeplink_ext_get_platform_type()
 if platform_type == PlatformType.TORCH_NPU:
-    from ._rotary_embedding_npu import ApplyRotaryEmb
+    # from ._rotary_embedding_npu import ApplyRotaryEmb
+    from .rotary_embedding_fallback import ApplyRotaryEmbTorch as ApplyRotaryEmb
 elif platform_type == PlatformType.TORCH_DIPU:
     from ._rotary_embedding_dipu import ApplyRotaryEmb
 else:
