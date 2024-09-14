9eb52a22fd86a939b59995880f7dc5b0e688c3eb
wangxing <131418410+POI-WX@users.noreply.github.com>
2024-09-14 11:22:04 +0800

    fix: use combined rms norm for accuracy for dipu (#128)
    Use combined rms norm for accuracy for dipu.

--- deeplink_ext/interntrain_ops/rms_norm.py
+++ deeplink_ext/interntrain_ops/rms_norm.py
@@ -5,9 +5,12 @@ from deeplink_ext.utils import PlatformType, deeplink_ext_get_platform_type
 platform_type = deeplink_ext_get_platform_type()
 if platform_type == PlatformType.TORCH_NPU:
     # from ._mixed_rms_norm_npu import MixedFusedRMSNorm
+    # Due to the accuracy problem of the npu fused operator, a torch combination is used as an alternative.
     from .rms_norm_fallback import MixedRMSNormTorch as MixedFusedRMSNorm
 elif platform_type == PlatformType.TORCH_DIPU:
-    from ._mixed_rms_norm_dipu import MixedFusedRMSNorm
+    # from ._mixed_rms_norm_dipu import MixedFusedRMSNorm
+    # Due to the accuracy problem of the npu fused operator, a torch combination is used as an alternative.
+    from .rms_norm_fallback import MixedRMSNormTorch as MixedFusedRMSNorm
 else:
     raise ImportError
 
