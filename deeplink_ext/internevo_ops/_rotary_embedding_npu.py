d69d17b3b45240279f2692dba63d70006e4e4f1b
POI-WX <wangxing2@sensetime.com>
2024-10-22 13:01:16 +0000

    use fused rope op from mindspeed

--- deeplink_ext/internevo_ops/_rotary_embedding_npu.py
+++ deeplink_ext/internevo_ops/_rotary_embedding_npu.py
@@ -3,41 +3,9 @@
 import torch
 import torch_npu
 from einops import rearrange, repeat
+from mindspeed.ops.npu_rotary_position_embedding import npu_rotary_position_embedding
 
 __all__ = ["ApplyRotaryEmb"]
-
-
-def rotate_half(x, interleaved=False):
-    if not interleaved:
-        x1, x2 = x.chunk(2, dim=-1)
-        return torch.cat((-x2, x1), dim=-1)
-    else:
-        x1, x2 = x[..., ::2], x[..., 1::2]
-        return rearrange(
-            torch.stack((-x2, x1), dim=-1), "... d two -> ... (d two)", two=2
-        )
-
-
-def apply_rotary_emb_torch(x, cos, sin, interleaved=False):
-    """
-    x: (batch_size, seqlen, nheads, headdim)
-    cos, sin: (seqlen, rotary_dim / 2) or (batch_size, seqlen, rotary_dim / 2)
-    """
-    ro_dim = cos.shape[-1] * 2
-    assert ro_dim <= x.shape[-1]
-    cos = repeat(
-        cos, "... d -> ... 1 (2 d)" if not interleaved else "... d -> ... 1 (d 2)"
-    )
-    sin = repeat(
-        sin, "... d -> ... 1 (2 d)" if not interleaved else "... d -> ... 1 (d 2)"
-    )
-    return torch.cat(
-        [
-            x[..., :ro_dim] * cos + rotate_half(x[..., :ro_dim], interleaved) * sin,
-            x[..., ro_dim:],
-        ],
-        dim=-1,
-    )
 
 
 # adpated from https://github.com/Dao-AILab/flash-attention/blob/main/flash_attn/layers/rotary.py#L35
@@ -72,8 +40,8 @@ class ApplyRotaryEmb(torch.autograd.Function):
         assert sin.shape == (rotary_seqlen, rotary_dim // 2)
 
         if interleaved:
-            cos = cos[:seqlen]
-            sin = sin[:seqlen]
+            cos = repeat(cos[:seqlen].unsqueeze(0).unsqueeze(2), "... d -> ... (d 2)")
+            sin = repeat(sin[:seqlen].unsqueeze(0).unsqueeze(2), "... d -> ... (d 2)")
         else:
             # "s d -> 1 s 1 d"
             cos = cos[:seqlen].unsqueeze(0).unsqueeze(2).repeat(1, 1, 1, 2)
@@ -82,12 +50,17 @@ class ApplyRotaryEmb(torch.autograd.Function):
         ctx.interleaved = interleaved
         ctx.in_place = in_place
         if interleaved:
-            out = apply_rotary_emb_torch(x, cos, sin, interleaved)
+            x_ro = x[..., :rotary_dim]
+            out_ro = npu_rotary_position_embedding(x_ro, cos, sin, 1)
             if in_place:
-                x.copy_(out)
+                x[..., :rotary_dim].copy_(out_ro)
                 return x
-            else:
+            if rotary_dim < head_dim:
+                out = torch.empty_like(x)
+                out[..., :rotary_dim].copy_(out_ro)
+                out[..., rotary_dim:].copy_(x[..., rotary_dim:])
                 return out
+            return out_ro
         else:
             x_ro = x[..., :rotary_dim]
             out_ro = torch_npu.npu_rotary_mul(x_ro, cos, sin)
@@ -107,14 +80,19 @@ class ApplyRotaryEmb(torch.autograd.Function):
         rotary_dim = cos.shape[-1]
         head_dim = grad_out.shape[-1]
         if ctx.interleaved:
-            grad_input = apply_rotary_emb_torch(
-                grad_out, cos, torch.neg(sin), ctx.interleaved
+            grad_out_ro = grad_out[..., :rotary_dim]
+            grad_input_ro = npu_rotary_position_embedding(
+                grad_out_ro, cos, torch.neg(sin), 1
             )
             if ctx.in_place:
-                grad_out.copy_(grad_input)
+                grad_out[..., :rotary_dim].copy_(grad_input_ro)
                 return grad_out, None, None, None, None
-            else:
+            if rotary_dim < head_dim:
+                grad_input = torch.empty_like(grad_out)
+                grad_input[..., :rotary_dim].copy_(grad_input_ro)
+                grad_input[..., rotary_dim:].copy_(grad_out[..., rotary_dim:])
                 return grad_input, None, None, None, None
+            return grad_input_ro, None, None, None, None
         else:
             grad_out_ro = grad_out[..., :rotary_dim]
             grad_input_ro = torch_npu.npu_rotary_mul(grad_out_ro, cos, torch.neg(sin))
