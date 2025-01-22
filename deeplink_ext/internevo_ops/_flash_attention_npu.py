ee852e4192835880f863d25409a3c638bf1e0830
wangxing2 <wangxing2@sensetime.com>
2025-01-22 16:06:29 +0800

    fix

--- deeplink_ext/internevo_ops/_flash_attention_npu.py
+++ deeplink_ext/internevo_ops/_flash_attention_npu.py
@@ -32,17 +32,13 @@ def flash_attn_func(
     seqlen_k = k.shape[1]
     head_num = q.shape[-2]
 
-    if seqlen_q == seqlen_k and seqlen_q < 2048 and seqlen_k < 2048:
-        sparse_mode = 0
-    else:
-        sparse_mode = 2
-
-    seqlen_q = min(seqlen_q, 2048)
-    seqlen_k = min(seqlen_k, 2048)
+    assert seqlen_q == seqlen_k
+    sparse_mode = 2 if seqlen_q > 2048 else 0
+    seqlen = min(seqlen_q, 2048)
 
     attention_mask = (
         torch.triu(
-            torch.ones([seqlen_q, seqlen_k], dtype=torch.bool, device=q.device),
+            torch.ones([seqlen, seqlen], dtype=torch.bool, device=q.device),
             diagonal=1,
         )
         if causal
@@ -81,7 +77,6 @@ def flash_attn_varlen_func(
     alibi_slopes=None,
     deterministic=False,
     return_attn_probs=False,
-    block_table=None,
 ):
     if softmax_scale is None:
         softmax_scale = q.shape[-1] ** (-0.5)
@@ -89,17 +84,14 @@ def flash_attn_varlen_func(
 
     cu_seqlens_q = cu_seqlens_q[1:].tolist()
     cu_seqlens_k = cu_seqlens_k[1:].tolist()
-    seqlen_q = min(max_seqlen_q, 2048)
-    seqlen_k = min(max_seqlen_k, 2048)
 
-    if max_seqlen_q < 2048:
-        sparse_mode = 0
-    else:
-        sparse_mode = 2
+    assert max_seqlen_q == max_seqlen_k
+    sparse_mode = 2 if max_seqlen_q > 2048 else 0
+    max_seqlen = min(max_seqlen_q, 2048)
 
     attention_mask = (
         torch.triu(
-            torch.ones([seqlen_q, seqlen_k], dtype=torch.bool, device=q.device),
+            torch.ones([max_seqlen, max_seqlen], dtype=torch.bool, device=q.device),
             diagonal=1,
         )
         if causal
@@ -114,8 +106,8 @@ def flash_attn_varlen_func(
         "TND",
         atten_mask=attention_mask,
         scale=softmax_scale,
-        pre_tockens=q.shape[0],  # seq_len
-        next_tockens=0,  # 0
+        pre_tockens=q.shape[0],
+        next_tockens=0,
         keep_prob=1 - dropout_p,
         sparse_mode=sparse_mode,
         actual_seq_qlen=cu_seqlens_q,
@@ -143,16 +135,12 @@ def flash_attn_qkvpacked_func(
     seqlen_qkv = qkv.shape[1]
     head_num = q.shape[-2]
 
-    if seqlen_qkv < 2048:
-        sparse_mode = 0
-    else:
-        sparse_mode = 2
-
-    seqlen_qkv = min(qkv.shape[1], 2048)
+    sparse_mode = 2 if seqlen_qkv > 2048 else 0
+    seqlen = min(seqlen_qkv, 2048)
 
     attention_mask = (
         torch.triu(
-            torch.ones([seqlen_qkv, seqlen_qkv], dtype=torch.bool, device=q.device),
+            torch.ones([seqlen, seqlen], dtype=torch.bool, device=q.device),
             diagonal=1,
         )
         if causal
@@ -192,21 +180,17 @@ def flash_attn_kvpacked_func(
     k = kv[:, :, 0]
     v = kv[:, :, 1]
 
-    s0 = q.shape[1]
-    s1 = kv.shape[1]
+    seqlen_q = q.shape[1]
+    seqlen_kv = kv.shape[1]
     head_num = q.shape[-2]
 
-    if s0 == s1 and s0 < 2048 and s1 < 2048:
-        sparse_mode = 0
-    else:
-        sparse_mode = 2
-
-    seqlen_q = min(s0, 2048)
-    seqlen_k = min(s1, 2048)
+    assert seqlen_q == seqlen_kv
+    sparse_mode = 2 if seqlen_q > 2048 else 0
+    seqlen = min(seqlen_q, 2048)
 
     attention_mask = (
         torch.triu(
-            torch.ones([seqlen_q, seqlen_k], dtype=torch.bool, device=q.device),
+            torch.ones([seqlen, seqlen], dtype=torch.bool, device=q.device),
             diagonal=1,
         )
         if causal
@@ -222,7 +206,7 @@ def flash_attn_kvpacked_func(
         atten_mask=attention_mask,
         scale=softmax_scale,
         keep_prob=1 - dropout_p,
-        pre_tockens=seqlen_k,
+        pre_tockens=seqlen_q,
         next_tockens=0,
         sparse_mode=sparse_mode,
     )[0]
@@ -247,32 +231,32 @@ def flash_attn_varlen_qkvpacked_func(
     q = qkv[:, 0]
     k = qkv[:, 1]
     v = qkv[:, 2]
-    n = q.shape[1]
-    if max_seqlen > 2048:
-        sparse_mode = 2
-    else:
-        sparse_mode = 0
+    head_num = q.shape[1]
+
     cu_seqlens_q = cu_seqlens[1:].tolist()
     cu_seqlens_k = cu_seqlens[1:].tolist()
-    seqlen = min(max_seqlen, 2048)
+
+    sparse_mode = 2 if max_seqlen > 2048 else 0
+    max_seqlen = min(max_seqlen, 2048)
     attention_mask = (
         torch.triu(
-            torch.ones([seqlen, seqlen], dtype=torch.bool, device=q.device),
+            torch.ones([max_seqlen, max_seqlen], dtype=torch.bool, device=q.device),
             diagonal=1,
         )
         if causal
         else None
     )
+
     out = torch_npu.npu_fusion_attention(
         q,
         k,
         v,
-        n,
+        head_num,
         "TND",
         atten_mask=attention_mask,
         scale=softmax_scale,
-        pre_tockens=q.shape[0],  # seq_len
-        next_tockens=0,  # 0
+        pre_tockens=q.shape[0],
+        next_tockens=0,
         keep_prob=1 - dropout_p,
         sparse_mode=sparse_mode,
         actual_seq_qlen=cu_seqlens_q,
@@ -300,35 +284,33 @@ def flash_attn_varlen_kvpacked_func(
         softmax_scale = q.shape[-1] ** (-0.5)
     k = kv[:, 0]
     v = kv[:, 1]
-    n = q.shape[1]
+    head_num = q.shape[1]
     cu_seqlens_q = cu_seqlens_q[1:].tolist()
     cu_seqlens_k = cu_seqlens_k[1:].tolist()
-    seqlen_q = min(max_seqlen_q, 2048)
-    seqlen_k = min(max_seqlen_k, 2048)
 
-    if max_seqlen_q > 2048:
-        sparse_mode = 2
-    else:
-        sparse_mode = 0
+    assert max_seqlen_q == max_seqlen_k
+    sparse_mode = 2 if max_seqlen_q > 2048 else 0
+    max_seqlen = min(max_seqlen_q, 2048)
 
     attention_mask = (
         torch.triu(
-            torch.ones([seqlen_q, seqlen_k], dtype=torch.bool, device=q.device),
+            torch.ones([max_seqlen, max_seqlen], dtype=torch.bool, device=q.device),
             diagonal=1,
         )
         if causal
         else None
     )
+
     out = torch_npu.npu_fusion_attention(
         q,
         k,
         v,
-        n,
+        head_num,
         "TND",
         atten_mask=attention_mask,
         scale=softmax_scale,
-        pre_tockens=q.shape[0],  # seq_len
-        next_tockens=0,  # 0
+        pre_tockens=q.shape[0],
+        next_tockens=0,
         keep_prob=1 - dropout_p,
         sparse_mode=sparse_mode,
         actual_seq_qlen=cu_seqlens_q,
